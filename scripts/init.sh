7a206f365b723bbb29632cf947c6c23a05946ad8
cethien <borislaw.sotnikow@gmx.de>
2024-08-14 16:14:13 +0200

    init: add failsafes for go

--- scripts/init.sh
+++ scripts/init.sh
@@ -12,28 +12,42 @@ function edit() {
     done
 }
 
+if [ "$1" = "go" ]; then
+    if ! command -v go &>/dev/null; then
+        echo "go could not be found"
+        echo "please install go"
+        exit 1
+    fi
+fi
+
 if [ $# -eq 1 ]; then
     if [ "$1" = "go" ]; then
         echo "no module name provided"
         return 1
     else
-        mkdir "$1" && cd "$_" && touch README.md .gitignore && \
-        edit README.md && \
-        git_init
+        mkdir "$1" && cd "$_" && touch README.md .gitignore &&
+            edit README.md &&
+            git_init
         return 0
     fi
 elif [ $# -eq 2 ]; then
     if [ "$1" = "go" ]; then
-        gonew github.com/cethien/template-go@latest $2 && \
-        module_name=$(basename "$2") && \
-        trimmed_module_name=$(echo "$module_name" | sed 's/[-_]//g') && \
-        command cd "$module_name"
-        command find . -type f -exec sed -i "s/template-go/$module_name/g" {} + && \
-        command find . -type f -exec sed -i "s/templatego/$trimmed_module_name/g" {} + && \
-        mv template-go.go "$module_name.go" && \
-        mv cmd/template-go cmd/"$module_name" && \
-        edit README.md .goreleaser.yml && \
-        git_init
+        if ! command -v gonew &>/dev/null; then
+            echo "gonew could not be found"
+            echo "installing"
+            go install golang.org/x/tools/cmd/gonew@latest
+        fi
+
+        gonew github.com/cethien/template-go@latest "$2" &&
+            module_name=$(basename "$2") &&
+            trimmed_module_name=$(echo "$module_name" | sed 's/[-_]//g') &&
+            command cd "$module_name" || exit
+
+        command find . -type f -exec sed -i "s/template-go/$module_name/g" {} + &&
+            command find . -type f -exec sed -i "s/templatego/$trimmed_module_name/g" {} + &&
+            mv cmd/template-go cmd/"$module_name" &&
+            edit README.md .goreleaser.yml &&
+            git_init
         return 0
     else
         echo "Illegal number of parameters"
