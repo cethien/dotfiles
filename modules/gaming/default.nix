9f237ab96af1f00721a80c1086699b638120586d
cethien <borislaw.sotnikow@gmx.de>
2024-12-11 14:54:48 +0100

    graphics settings

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -6,11 +6,6 @@
     ./steam.nix
   ];
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
   gaming.nvidia.enable = system.profile.isHomePC;
   gaming.steam.enable = system.profile.isHomePC;
 }
--- modules/gaming/nvidia.nix
+++ modules/gaming/nvidia.nix
@@ -4,8 +4,8 @@
   options.gaming.nvidia.enable = lib.mkEnableOption "Enable Nvidia GPU support";
 
   config = lib.mkIf config.gaming.nvidia.enable {
-    hardware.nvidia.modesetting.enable = true;
-    hardware.nvidia.open = true;
     services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia.open = true;
+    hardware.nvidia.modesetting.enable = true;
   };
 }
