f278aac01b738ac1c6b21bcfb491e97f5831fe6f
cethien <borislaw.sotnikow@gmx.de>
2024-12-12 16:39:39 +0100

    graphics

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -6,6 +6,11 @@
     ./steam.nix
   ];
 
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
   gaming.nvidia.enable = system.profile.isHomePC;
   gaming.steam.enable = system.profile.isHomePC;
 }
