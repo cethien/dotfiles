26e97ac1c1c88fc853a184e6d2022a189f9ce51e
cethien <borislaw.sotnikow@gmx.de>
2024-12-18 02:11:33 +0100

    added yazi + other tools

--- modules/home/cethien/sh/default.nix
+++ modules/home/cethien/sh/default.nix
@@ -10,16 +10,20 @@
     ./eza.nix
     ./fastfetch.nix
     ./fd.nix
+    ./ffmpeg.nix
     ./fzf.nix
     ./gh.nix
     ./git.nix
+    ./jq.nix
     ./lazydocker.nix
     ./neovim.nix
     ./oh-my-posh.nix
+    ./poppler.nix
     ./procs.nix
     ./ripgrep.nix
     ./ssh.nix
     ./tmux.nix
+    ./yazi.nix
     ./zoxide.nix
   ];
 }
