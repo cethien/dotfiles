e6dc20963847f9badefe3535ad88d78d86a1506b
cethien <borislaw.sotnikow@gmx.de>
2025-01-22 19:46:32 +0100

    home: added helix keymaps for line moving

--- modules/home/programs/helix/default.nix
+++ modules/home/programs/helix/default.nix
@@ -311,6 +311,8 @@ in {
           esc = ["collapse_selection" "keep_primary_selection"];
           C-s = ":w";
           C-g = [":new" ":insert-output lazygit" ":buffer-close!" ":redraw"];
+          A-down = ["extend_to_line_bounds" "delete_selection" "paste_after"];
+          A-up = ["extend_to_line_bounds" "delete_selection" "move_line_up" "paste_before"];
         };
       };
     };
