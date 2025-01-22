7d800b4ada1f94ae72865239332342a9069bc8e6
cethien <borislaw.sotnikow@gmx.de>
2025-01-22 22:05:53 +0100

    home: helix keybindings

--- modules/home/programs/helix/keys.nix
+++ modules/home/programs/helix/keys.nix
@@ -1,11 +1,13 @@
 {
   normal = {
+    esc = [ "collapse_selection" "keep_primary_selection" ];
     space.space = "file_picker";
     space.q = ":q";
-    esc = [ "collapse_selection" "keep_primary_selection" ];
     C-s = ":w";
-    C-g = [ ":new" ":insert-output lazygit" ":buffer-close!" ":redraw" ];
+
     A-down = [ "extend_to_line_bounds" "delete_selection" "paste_after" ];
     A-up = [ "extend_to_line_bounds" "delete_selection" "move_line_up" "paste_before" ];
+
+    C-S-g = [ ":new" ":insert-output lazygit" ":buffer-close!" ":redraw" ];
   };
 }
