55229e5f3dadaa844cc59ac198d2df51c84b6669
cethien <borislaw.sotnikow@gmx.de>
2025-02-20 01:16:15 +0100

    neovim: plugins, configs, keymaps
    more ide like features

--- modules/home/programs/neovim/default.nix
+++ modules/home/programs/neovim/default.nix
@@ -13,8 +13,13 @@ in
   };
 
   config = mkIf cfg.enable {
+    home.shellAliases.v = "nvim";
+
     programs.nixvim = {
       enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
 
       globalOpts = {
         number = true;
@@ -30,8 +35,6 @@ in
         splitbelow = true;
 
         list = true;
-
-
       };
 
       colorschemes.catppuccin.enable = true;
@@ -40,14 +43,20 @@ in
         web-devicons.enable = true;
         lualine.enable = true;
         bufferline.enable = true;
-
+        dropbar.enable = true;
+        image.enable = true;
+        overseer.enable = true;
         blink-cmp.enable = true;
+        commentary.enable = true;
+        dap.enable = true;
+        cursorline.enable = true;
+        telescope.enable = true;
 
         direnv.enable = true;
         nix.enable = true;
         nix-develop.enable = true;
 
-        image.enable = true;
+        # hex.enable = true;
 
         lazygit.enable = true;
         yazi.enable = true;
@@ -61,8 +70,6 @@ in
 
         treesitter.enable = true;
         rest.enable = true;
-
-        oil.enable = true;
 
         persistence.enable = true;
 
@@ -93,7 +100,6 @@ in
             cssls.enable = true;
             htmx.enable = true;
             tailwindcss.enable = true;
-
             svelte.enable = true;
 
             gopls.enable = true;
@@ -104,13 +110,39 @@ in
 
       keymaps = [
         {
-          action = "<cmd>LazyGit<cr>";
           key = "<C-S-g>";
+          action = "<cmd>LazyGit<cr>";
         }
         {
-          action = "<cmd>Commentary<CR>";
-          key = "<leader>/";
+          mode = "n";
+          key = "<C-p>";
+          action = "<cmd>OverseerRun<cr>";
         }
+
+        {
+          mode = "n";
+          key = "<C-o>";
+          action = "<cmd>OverseerToggle<cr>";
+        }
+
+        {
+          mode = "n";
+          key = "<C-s>";
+          action = "<cmd>write<CR>";
+        }
+
+        {
+          mode = "n";
+          key = "<C-Space>";
+          action = "<cmd>Telescope find_files<cr>";
+        }
+
+        {
+          mode = "n";
+          key = "<C-c>";
+          action = "<cmd>Commentary<CR>";
+        }
+
         {
           mode = "n";
           key = "<Tab>";
@@ -119,7 +151,6 @@ in
             desc = "Cycle to next buffer";
           };
         }
-
         {
           mode = "n";
           key = "<S-Tab>";
@@ -128,31 +159,21 @@ in
             desc = "Cycle to previous buffer";
           };
         }
-
         {
           mode = "n";
-          key = "<S-l>";
-          action = "<cmd>BufferLineCycleNext<cr>";
-          options = {
-            desc = "Cycle to next buffer";
-          };
-        }
-
-        {
-          mode = "n";
-          key = "<S-h>";
-          action = "<cmd>BufferLineCyclePrev<cr>";
-          options = {
-            desc = "Cycle to previous buffer";
-          };
-        }
-
-        {
-          mode = "n";
-          key = "<leader>bd";
+          key = "<C-w>";
           action = "<cmd>bdelete<cr>";
           options = {
             desc = "Delete buffer";
+          };
+        }
+
+        {
+          mode = "n";
+          key = "<C-S-5>";
+          action = "<cmd>terminal<cr>";
+          options = {
+            desc = "open terminal";
           };
         }
       ];
