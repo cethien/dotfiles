c7d54950f25f5838e181a028953a6397ee03628a
cethien <borislaw.sotnikow@gmx.de>
2025-05-12 21:12:57 +0200

    fix: waybar option

--- modules/home/programs/hyprland/programs/waybar/default.nix
+++ modules/home/programs/hyprland/programs/waybar/default.nix
@@ -1,11 +1,11 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, ... }:
 let
   inherit (lib) mkIf mkEnableOption;
   cfg = config.deeznuts.programs.waybar;
   enabled = cfg.enable;
 in
 {
-  options.deeznuts.programs.waybar.enable = mkEnableOption;
+  options.deeznuts.programs.waybar.enable = mkEnableOption "waybar";
 
   config = mkIf enabled {
     programs.waybar = {
