f47f0903222925997a242d5a84c25608d1bd9ec3
cethien <borislaw.sotnikow@gmx.de>
2025-05-13 03:00:03 +0200

    hyprland: reorder, binds

--- modules/home/programs/hyprland/programs/default.nix
+++ modules/home/programs/hyprland/programs/default.nix
@@ -6,15 +6,15 @@ let
 in
 {
   imports = [
-    ./kitty
+    ./kitty.nix
     ./waybar
     ./rofi
-    ./clipse
+    ./clipse.nix
     ./common-gui
-    ./zathura
-    ./hypridle
-    ./hyprlock
-    ./hyprpaper
+    ./zathura.nix
+    ./hypridle.nix
+    ./hyprlock.nix
+    ./hyprpaper.nix
     ./hyprpanel
     ./hyprshot
     ./wf-recorder
@@ -25,6 +25,7 @@ in
       brightnessctl
       hyprpicker
       udiskie
+      playerctl
     ];
 
     deeznuts.programs = {
@@ -51,9 +52,21 @@ in
         "SUPER SHIFT, C, exec, hyprpicker -a"
       ];
 
+      binde = [
+        ", XF86MonBrightnessUp, exec, brightnessctl s 5%+"
+        ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
+
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.2 @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1.2 @DEFAULT_AUDIO_SINK@ 5%-"
+      ];
+
       bindl = [
-        ", XF86MonBrightnessUp, exec, brightnessctl s 10%+"
-        ", XF86MonBrightnessDown, exec, brightnessctl s 10%-"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPrev, exec, playerctl previous"
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+
+        ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
       ];
     };
   };
