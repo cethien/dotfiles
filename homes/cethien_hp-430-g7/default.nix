2413300389bc4a675cd0811c86bbfc8d8a370fc9
cethien <borislaw.sotnikow@gmx.de>
2025-05-22 23:14:04 +0200

    lpt hyprland settings, games
    my tower broke :(

--- homes/cethien_hp-430-g7/default.nix
+++ homes/cethien_hp-430-g7/default.nix
@@ -10,16 +10,32 @@
       hyprland = {
         enable = true;
         hypridle.enable = true;
+        monitors = [
+          "eDP-1, 1920x1080@60, 0x0, 1.2"
+          "DP-1, 1920x1080@144, 1920x0, 1.2"
+        ];
 
-        programs = {
-          spotify.autostart.enable = true;
-        };
+        workspaces = [
+          "1, monitor:eDP-1, persistent:true"
+          "2, monitor:eDP-1, persistent:true"
+          "3, monitor:eDP-1, persistent:true"
+          "4, monitor:eDP-1, persistent:true"
+          "5, monitor:eDP-1, persistent:true"
+          "6, monitor:eDP-1, persistent:true"
+
+          "7, monitor:DP-1, persistent:true"
+          "8, monitor:DP-1, persistent:true"
+        ];
       };
 
       cli.enable = true;
+
       basic.enable = true;
-      vscode.enable = true;
-      obs-studio.enable = true;
+      zen-browser.hyprland.autostart.enable = true;
+      spotify.hyprland.autostart.enable = true;
+
+      retroarch.enable = true;
+      pokemmo.enable = true;
     };
   };
 }
