d5f2351bcda93f50dd04427db261214105b7201a
cethien <borislaw.sotnikow@gmx.de>
2025-05-22 23:10:04 +0200

    jetbrains: simplified

--- modules/home/programs/jetbrains/default.nix
+++ modules/home/programs/jetbrains/default.nix
@@ -1,6 +1,30 @@
+{ lib, config, pkgs, ... }:
+let
+  inherit (lib) mkEnableOption mkOption types mkIf mkMerge;
+  cfg = config.deeznuts.programs.jetbrains;
+in
 {
-  imports = [
-    ./idea-community.nix
-    ./rider.nix
-  ];
+  options.deeznuts.programs.jetbrains = {
+    idea.enable = mkEnableOption "Jetbrains IntelliJ Community IDE";
+    rider.enable = mkEnableOption "Jetbrains Rider IDE";
+    hyprland.workspace = mkOption {
+      type = types.int;
+      default = config.deeznuts.programs.hyprland.defaultWorkspaces.development;
+      description = "default hyprland workspace";
+    };
+  };
+
+  config = {
+    wayland.windowManager.hyprland.settings = {
+      windowrulev2 = [
+        "workspace ${toString cfg.hyprland.workspace}, class:jetbrains-idea-ce"
+        "workspace ${toString cfg.hyprland.workspace}, class:jetbrains-rider"
+      ];
+    };
+
+    home.packages = mkMerge [
+      (mkIf cfg.idea.enable [ pkgs.jetbrains.idea-community ])
+      (mkIf cfg.rider.enable [ pkgs.jetbrains.rider ])
+    ];
+  };
 }
