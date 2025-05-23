375d8d07894fbb145b698d9904922b95476980cd
cethien <borislaw.sotnikow@gmx.de>
2025-05-24 00:24:15 +0200

    zoxide workaround rm
    was fixed

--- modules/home/programs/zoxide/default.nix
+++ modules/home/programs/zoxide/default.nix
@@ -1,24 +1,17 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, ... }:
 let
   inherit (lib) mkIf mkEnableOption;
   cfg = config.deeznuts.programs.zoxide;
 in
 {
   options.deeznuts.programs.zoxide = {
-    enable = mkEnableOption "Enable zoxide";
+    enable = mkEnableOption "zoxide";
   };
 
   config = mkIf cfg.enable {
     programs.zoxide = {
       enable = true;
-      enableBashIntegration = false;
       options = [ "--cmd cd" ];
     };
-
-    programs.bash.initExtra =
-      lib.mkOrder 2000 # sh
-        ''
-          eval "$(${lib.getExe pkgs.zoxide} init bash --cmd cd)"
-        '';
   };
 }
