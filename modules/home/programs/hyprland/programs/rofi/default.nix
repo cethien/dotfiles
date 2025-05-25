e9114a6129e308aacbbf6150adc7728f25178184
cethien <borislaw.sotnikow@gmx.de>
2025-05-25 21:04:58 +0200

    rofimoji

--- modules/home/programs/hyprland/programs/rofi/default.nix
+++ modules/home/programs/hyprland/programs/rofi/default.nix
@@ -1,16 +1,21 @@
-{ lib, config, pkgs, ... }:
-let
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
   inherit (lib) mkIf mkEnableOption;
   cfg = config.deeznuts.programs.rofi;
   enable = cfg.enable;
-in
-{
+in {
   options.deeznuts.programs.rofi = {
     enable = mkEnableOption "rofi";
   };
 
   config = mkIf enable {
     home.packages = with pkgs; [
+      rofimoji
+
       rofi-power-menu
       rofi-bluetooth
 
@@ -26,19 +31,23 @@ in
       package = pkgs.rofi-wayland;
       terminal = "${pkgs.kitty}/bin/kitty";
       theme = ./theme.rafi;
-      plugins = with pkgs;[
+      plugins = with pkgs; [
         rofi-calc
-        rofi-emoji
       ];
       extraConfig = {
         show-icons = true;
       };
     };
 
+    home.file.".config/rofi/grid.rasi".source = ./grid.rasi;
+
     wayland.windowManager.hyprland.settings = {
       bind = [
         "SUPER, Space, exec, rofi -show drun"
-        "SUPER, PERIOD, exec, rofi -show calc -modi calc -no-show-match -no-sort -no-bold -no-history"
+        "SUPER, comma, exec, rofi -show calc -modi calc -no-show-match -no-sort -no-bold -no-history"
+        ''
+          SUPER, PERIOD, exec, rofimoji --hidden-description --selector-args="-theme ~/.config/rofi/grid.rasi -kb-row-left Left -kb-row-right Right -kb-move-char-back Control+b -kb-move-char-forward Control+f"
+        ''
         ''
           SUPER, escape, exec, rofi -show power-menu -modi "power-menu:rofi-power-menu --choices=shutdown/reboot/suspend"
         ''
