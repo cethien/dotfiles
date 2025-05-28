4e963b93394185e334d895895951fa725e9c6efe
cethien <borislaw.sotnikow@gmx.de>
2025-05-28 02:11:33 +0200

    rm hyprpaper preloads
    handled by stylix

--- modules/home/programs/hyprland/programs/hyprpaper.nix
+++ modules/home/programs/hyprland/programs/hyprpaper.nix
@@ -1,27 +1,15 @@
-{ lib, config, ... }:
-let
+{
+  lib,
+  config,
+  ...
+}: let
   inherit (lib) mkIf mkEnableOption;
   cfg = config.deeznuts.programs.hyprpaper;
   enabled = cfg.enable;
-in
-{
+in {
   options.deeznuts.programs.hyprpaper.enable = mkEnableOption "hyprpaper";
 
   config = mkIf enabled {
-    services.hyprpaper = {
-      enable = true;
-
-      settings = {
-        preload = [
-          "/home/cethien/Pictures/wallpapers/pixelart-mountains.png"
-          "/home/cethien/Pictures/wallpapers/drippy-smiley-solid-color.jpg"
-        ];
-
-        wallpaper = [
-          "DP-1,/home/cethien/Pictures/wallpapers/pixelart-mountains.png"
-          "HDMI-A-1,/home/cethien/Pictures/wallpapers/drippy-smiley-solid-color.jpg"
-        ];
-      };
-    };
+    services.hyprpaper.enable = true;
   };
 }
