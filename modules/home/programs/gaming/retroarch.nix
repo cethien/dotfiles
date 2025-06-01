6fbbfbc9d17576973530a68b05f5daa346e41296
cethien <borislaw.sotnikow@gmx.de>
2025-06-01 17:03:20 +0200

    retroarch sync

--- modules/home/programs/gaming/retroarch.nix
+++ modules/home/programs/gaming/retroarch.nix
@@ -23,6 +23,44 @@ in {
       ];
     };
 
+    services.syncthing.settings = mkIf config.services.syncthing.enable {
+      folders.retroarch = {
+        id = "retroach";
+        path = "${config.home.homeDirectory}/.config/retroarch";
+        devices = ["cethien.me"];
+      };
+    };
+
+    home.file = mkIf config.services.syncthing.enable {
+      ".config/retroarch/.stignore".text = ''
+        assets
+        autoconfig
+        config
+        !config/remaps/
+        !config/remaps/**
+        content_favorites.lpl
+        content_history.lpl
+        content_image_history.lpl
+        content_music_history.lpl
+        content_video_history.lpl
+        cores
+        database
+        !database/rdb/
+        !database/rdb/**
+        downloads
+        filters
+        logs
+        overlays
+        playlists
+        records
+        records_config
+        retroarch.cfg
+        screenshots
+        shaders
+        thumbnails
+      '';
+    };
+
     home.packages = let
       retroarchWithCores = pkgs.retroarch.withCores (cores:
         with cores; [
