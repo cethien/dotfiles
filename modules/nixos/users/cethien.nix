83fe1a32e6bc9cee41f4b10d09cee6dd9a3ff0ac
cethien <borislaw.sotnikow@gmx.de>
2025-06-08 15:27:30 +0200

    fix ssh user check

--- modules/nixos/users/cethien.nix
+++ modules/nixos/users/cethien.nix
@@ -18,7 +18,7 @@ in {
 
       hashedPassword = "$y$j9T$OqozgiTqzcpfQ9zbKkhvr0$i4lBjUVMF1xpGCOm57m/jXVWt0KVDh/evHHFq9mF7Z4";
 
-      openssh.authorizedKeys.keys = mkIf config.deeznuts.services.ssh.enable [
+      openssh.authorizedKeys.keys = mkIf config.services.openssh.enable [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKgrZmsUHZn7BAGAl83RUkNejlhJbmLr3lklrlVzy2Zz borislaw.sotnikow@gmx.de"
       ];
     };
