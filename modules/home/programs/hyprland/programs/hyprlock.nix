e465f6484acee6ad22f26425fdfafa52b84f4fc3
cethien <borislaw.sotnikow@gmx.de>
2025-07-04 11:38:42 +0200

    hyprlock color var

--- modules/home/programs/hyprland/programs/hyprlock.nix
+++ modules/home/programs/hyprland/programs/hyprlock.nix
@@ -7,6 +7,18 @@
 with lib; let
   cfg = config.deeznuts.programs.hyprland.hyprlock;
   enable = config.deeznuts.programs.hyprland.enable;
+
+  colors = {
+    base01 = "rgba(31, 29, 46, 0.6)"; # base01 - Darker Plum/Gray, subtle transparency
+    base02 = "rgba(38, 35, 53, 1.0)"; # base02 - Mid-Dark Plum/Gray, default ring (subtle)
+    base04 = "rgba(110, 106, 134, 1.0)"; # base04 - Muted Grey, soft hint
+    base05 = "rgba(224, 222, 244, 1.0)"; # base05 - Pale Lavender White, clear and readable
+    base08 = "rgba(235, 111, 146, 1.0)"; # base08 - Rosewater/Pink Accent for a cozy pop!
+    base09 = "rgba(246, 193, 119, 1.0)"; # base09 - Gold/Orange for Caps Lock (warning)
+    base0B = "rgba(49, 116, 143, 1.0)"; # base0B - Iris/Teal, calming clear input
+    base0D = "rgba(196, 167, 231, 1.0)"; # base0D - Lilac/Lavender for virtual keyboard
+    base0E = "rgba(231, 130, 132, 1.0)"; # base0E - Red Accent for errors
+  };
 in {
   options.deeznuts.programs.hyprland.hyprlock = {
     monitor = mkOption {
@@ -61,23 +73,18 @@ in {
             fade_on_empty = false;
             font_family = config.stylix.fonts.monospace.name;
 
-            # The main color of the "Enter Password" text when nothing is typed
-            placeholder_color = "rgba(110, 106, 134, 1.0)"; # base04 - Muted Grey, soft hint
+            placeholder_color = colors.base04;
+            bar_text_color = colors.base01;
+            bar_color = colors.base01;
+            fail_color = colors.base0E;
 
-            # Color of the text you type (Inner Font)
-            bar_text_color = "rgba(224, 222, 244, 1.0)"; # base05 - Pale Lavender White, clear and readable
+            ring_color = colors.base02;
 
-            # Color of the background of the input bar
-            bar_color = "rgba(31, 29, 46, 0.6)"; # base01 - Darker Plum/Gray, subtle transparency
-
-            # Ring colors (Outer Ring)
-            fail_color = "rgba(231, 130, 132, 1.0)"; # base0E - Red Accent for errors
-            ring_color = "rgba(38, 35, 53, 1.0)"; # base02 - Mid-Dark Plum/Gray, default ring (subtle)
-            ring_color_error = "rgba(231, 130, 132, 1.0)"; # base0E - Red for error ring
-            ring_color_clear = "rgba(49, 116, 143, 1.0)"; # base0B - Iris/Teal, calming clear input
-            ring_color_caps = "rgba(246, 193, 119, 1.0)"; # base09 - Gold/Orange for Caps Lock (warning)
-            ring_color_vkey = "rgba(196, 167, 231, 1.0)"; # base0D - Lilac/Lavender for virtual keyboard
-            ring_color_verify = "rgba(49, 116, 143, 1.0)"; # base0B - Iris/Teal, calming verify
+            ring_color_error = colors.base0E;
+            ring_color_clear = colors.base0B;
+            ring_color_caps = colors.base09;
+            ring_color_vkey = colors.base0D;
+            ring_color_verify = colors.base0B;
 
             placeholder_text = "enter password";
 
@@ -91,7 +98,7 @@ in {
           [
             {
               monitor = cfg.monitor;
-              color = "rgba(224, 222, 244, 1.0)"; # base05 - Pale Lavender White, main text color
+              color = colors.base05;
               font_family = config.stylix.fonts.sansSerif.name;
               font_size = "95";
               halign = "center";
@@ -102,7 +109,7 @@ in {
 
             {
               monitor = cfg.monitor;
-              color = "rgba(224, 222, 244, 1.0)"; # base05 - Pale Lavender White, main text color
+              color = colors.base05;
               font_family = config.stylix.fonts.sansSerif.name;
               font_size = "22";
               halign = "center";
@@ -114,7 +121,7 @@ in {
           (mkIf cfg.showBattery [
             {
               monitor = cfg.monitor;
-              color = "rgba(235, 111, 146, 1.0)"; # base08 - Rosewater/Pink Accent for a cozy pop!
+              color = colors.base08;
               font_family = config.stylix.fonts.sansSerif.name;
               font_size = "24";
               halign = "center";
