0070ce39a65f0e1e5be988114741d7fe7d4eddd3
cethien <borislaw.sotnikow@gmx.de>
2025-07-09 17:00:59 +0200

    hyprshot fixup

--- modules/home/programs/hyprland/programs/hyprshot/default.nix
+++ modules/home/programs/hyprland/programs/hyprshot/default.nix
@@ -1,28 +1,24 @@
-{ config, lib, pkgs, ... }:
-let
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
   inherit (lib) mkIf;
   enabled = config.deeznuts.programs.hyprland.enable;
-in
-{
+in {
   config = mkIf enabled {
-    home.packages = with pkgs; [
-      hyprshot
-      satty
-    ];
-
     home.file.".config/satty/config.toml".source = ./satty/config.toml;
 
     wayland.windowManager.hyprland.settings = {
-      bind =
-        let
-          hyprshot_base_cmd = "hyprshot --freeze --clipboard-only --raw";
-          satty_cmd = "satty --filename - --config ${config.home.homeDirectory}/.config/satty/config.toml";
-        in
-        [
-          ", Print, exec, ${hyprshot_base_cmd} -m output | ${satty_cmd}"
-          "ALT, Print, exec, ${hyprshot_base_cmd} -m window | ${satty_cmd}"
-          "SUPER SHIFT, S, exec, ${hyprshot_base_cmd} -m region | ${satty_cmd}"
-        ];
+      bind = let
+        hyprshot_base_cmd = "${pkgs.hyprshot}/bin/hyprshot --freeze --clipboard-only --raw";
+        satty_cmd = "${pkgs.satty}/bin/satty --filename - --config ${config.home.homeDirectory}/.config/satty/config.toml";
+      in [
+        ", Print, exec, ${hyprshot_base_cmd} -m output | ${satty_cmd}"
+        "ALT, Print, exec, ${hyprshot_base_cmd} -m window | ${satty_cmd}"
+        "SUPER SHIFT, S, exec, ${hyprshot_base_cmd} -m region | ${satty_cmd}"
+      ];
     };
   };
 }
