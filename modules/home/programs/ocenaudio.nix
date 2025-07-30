3d22a675413f92c9f58a510bb431d4396ffd5f87
cethien <borislaw.sotnikow@gmx.de>
2025-07-30 20:42:45 +0200

    oceanaudio -> audacity

--- modules/home/programs/ocenaudio.nix
+++ modules/home/programs/ocenaudio.nix
@@ -1,14 +1,17 @@
-{ lib, config, pkgs, ... }:
-let
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.deeznuts.programs.ocenaudio;
-in
-{
+in {
   options.deeznuts.programs.ocenaudio = {
-    enable = mkEnableOption "Enable ocenaudio";
+    enable = mkEnableOption "audacity";
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [ ocenaudio ];
+    home.packages = with pkgs; [audacity];
   };
 }
