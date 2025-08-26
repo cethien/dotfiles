81fc8b3de95ed077cf265650a30741beca987e72
cethien <borislaw.sotnikow@gmx.de>
2025-08-26 12:28:04 +0200

    added hollow crown to syncthing

--- modules/home/assets/default.nix
+++ modules/home/assets/default.nix
@@ -1,6 +1,19 @@
 {
+  config,
+  lib,
+  ...
+}:
+with lib; {
   home.file = {
     "Pictures/logo.png".source = ./logo.png;
     "Pictures/smiley.png".source = ./smiley.png;
+  };
+
+  services.syncthing.settings = mkIf config.services.syncthing.enable {
+    folders.hollow-crown = {
+      id = "hollow-crown";
+      path = "${config.home.homeDirectory}/projects/hollow-crown";
+      devices = ["xiaomi-15"];
+    };
   };
 }
