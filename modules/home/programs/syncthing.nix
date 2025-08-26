db257e2155013a187ec4f82bbc53896254acdf90
cethien <borislaw.sotnikow@gmx.de>
2025-08-26 12:26:53 +0200

    syncthing rm remote

--- modules/home/programs/syncthing.nix
+++ modules/home/programs/syncthing.nix
@@ -6,12 +6,6 @@
       "xiaomi-15" = {
         id = "RA74I3V-6MMZBHA-A6I7XCH-7HGDYPF-WDFNPZX-2WOO3OS-267B4MY-HL7VJA5";
       };
-      # "cethien.me" = {
-      #   id = "IMVEBRU-Q35QFHT-HLCNT7F-XKK7THW-3QJETCU-W2KHNZT-PJFOTDZ-MAZ7HAU";
-      #   addresses = [
-      #     "tcp://sync.cethien.me:22000"
-      #   ];
-      # };
     };
   };
 }
