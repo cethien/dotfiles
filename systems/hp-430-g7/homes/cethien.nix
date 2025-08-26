606c3282c642a04a7c9f8df65645b96fc81ee05e
cethien <borislaw.sotnikow@gmx.de>
2025-08-26 12:25:09 +0200

    lpt monitor settings

--- systems/hp-430-g7/homes/cethien.nix
+++ systems/hp-430-g7/homes/cethien.nix
@@ -12,20 +12,19 @@
       enable = true;
       hypridle.enable = true;
       monitors = [
-        "HDMI-A-1, 2560x1440@60, 1920x0, 1"
-        "eDP-1, 1920x1080@60, 0x1440, 1"
-        "DP-1, 1920x1080@60, 1920x1440, 1"
+        "HDMI-A-1, 2560x1440@60, 0x0, 1"
+        "eDP-1, 1920x1080@60, 320x1440, 1"
       ];
 
       workspaces = [
-        "1, monitor:DP-1, persistent:true" # browser
+        "1, monitor:HDMI-A-1, persistent:true" # browser
         "2, monitor:HDMI-A-1, persistent:true" # general
         "3, monitor:HDMI-A-1, persistent:true" # general
         "4, monitor:HDMI-A-1, persistent:true" # general
         "5, monitor:HDMI-A-1, persistent:true" # general
 
-        "6, monitor:DP-1, persistent:true" # video
-        "7, monitor:DP-1, persistent:true" # spotify
+        "6, monitor:HDMI-A-1, persistent:true" # video
+        "7, monitor:eDP-1, persistent:true" # spotify
         "8, monitor:DP-1" # btm
         "9, monitor:DP-1" # discord
         "10, monitor:DP-1" # obs
