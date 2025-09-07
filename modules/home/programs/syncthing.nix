343f1de22ba54a0a3692bc9ccefd5fa5fce1fb43
cethien <borislaw.sotnikow@gmx.de>
2025-09-07 17:13:09 +0200

    syncthing: add lpt

--- modules/home/programs/syncthing.nix
+++ modules/home/programs/syncthing.nix
@@ -1,10 +1,24 @@
 {
-  services.syncthing.enable = true;
-  services.syncthing.settings = {
-    options.urAccepted = -1;
-    devices = {
-      "xiaomi-15" = {
-        id = "RA74I3V-6MMZBHA-A6I7XCH-7HGDYPF-WDFNPZX-2WOO3OS-267B4MY-HL7VJA5";
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.deeznuts.programs.syncthing;
+in {
+  config = mkIf cfg.enable {
+    services.syncthing = {
+      enable = true;
+      settings = {
+        options.urAccepted = -1;
+        devices = {
+          "hp-430-g7" = {
+            id = "QQAPR6F-HZE4V4M-JTSTNLO-6SBXQFY-FTEH4ZQ-QDBFFXX-SL5SQOM-LD5XXQU";
+          };
+          "xiaomi-15" = {
+            id = "RA74I3V-6MMZBHA-A6I7XCH-7HGDYPF-WDFNPZX-2WOO3OS-267B4MY-HL7VJA5";
+          };
+        };
       };
     };
   };
