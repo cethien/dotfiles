bbc47c9dda0d4e7bfc21908d5179faa75923905a
cethien <borislaw.sotnikow@gmx.de>
2025-09-08 22:10:21 +0200

    gnome: enable coreapps

--- modules/nixos/desktop/gnome.nix
+++ modules/nixos/desktop/gnome.nix
@@ -15,7 +15,7 @@ in {
     services.displayManager.gdm.enable = true;
     services.desktopManager.gnome.enable = true;
 
-    services.gnome.core-apps.enable = false;
+    services.gnome.core-apps.enable = true;
     services.gnome.core-developer-tools.enable = false;
     services.gnome.games.enable = false;
     environment.gnome.excludePackages = with pkgs; [gnome-tour gnome-user-docs];
