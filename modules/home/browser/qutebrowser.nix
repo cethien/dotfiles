1ec71e94d755bcf046c1c614f9f6b8cfa48cb080
cethien <borislaw.sotnikow@gmx.de>
2025-10-06 00:18:37 +0200

    qutebrowser yazi filepicker

--- modules/home/browser/qutebrowser.nix
+++ modules/home/browser/qutebrowser.nix
@@ -110,6 +110,13 @@
         content = {
           notifications.enabled = false;
         };
+
+        fileselect = {
+          handler = "external";
+          single_file.command = ["kitty" "-e" "yazi" "--chooser-file={}"];
+          multiple_files.command = ["kitty" "-e" "yazi" "--chooser-file={}"];
+          folder.command = ["kitty" "-e" "yazi" "--chooser-file={}"];
+        };
       };
 
       loadAutoconfig = true;
