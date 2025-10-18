91cc29fb5916d5ad2277e0f7b215fcadba5530bd
cethien <borislaw.sotnikow@gmx.de>
2025-10-18 13:54:42 +0200

    reworked wifi script

--- modules/home/desktop/hyprland/rofi-wifi.sh
+++ modules/home/desktop/hyprland/rofi-wifi.sh
@@ -10,46 +10,39 @@ SEPARATOR="-----"
 OPT_SHOW_WIFI_INFO="  show wi-fi info"
 OPT_WIFI_ENABLE="󰖩  enable wi-fi"
 OPT_WIFI_DISABLE="󰖪  disable wi-fi"
-# OPT_HOTSPOT_ENABLE="󱜠  enable hotspot"
-# OPT_HOTSPOT_DISABLE="󱜡  disable hotspot"
 
 # ===== HELPERS =====
 show_wifi_info() {
   local IFACE="$WIFI_IFACE"
 
-  # IP and gateway
   local IP GW
   IP=$(nmcli -t -f IP4.ADDRESS dev show "$IFACE" | head -n1 | cut -d: -f2)
   GW=$(nmcli -t -f IP4.GATEWAY dev show "$IFACE" | head -n1 | cut -d: -f2)
 
-  # Signal, frequency, speed
   local line SIGNAL FREQ RATE
   line=$(nmcli -t -f IN-USE,SIGNAL,FREQ,RATE dev wifi list ifname "$IFACE" | awk -F: '$1=="*"{print $0}')
   IFS=: read -r _ SIGNAL FREQ RATE <<<"$line"
+  RATE="${RATE%% *}" # numeric only
 
-  # Remove extra units if needed
-  RATE="${RATE%% *}" # just the numeric part
-
-  # Show in terminal
   kitty -e bash -c "
 nmcli dev wifi show
 echo
-echo 'IP: ${IP:-N/A}'
-echo 'Gateway: ${GW:-N/A}'
-echo 'Signal: ${SIGNAL:-N/A} %'
-echo 'Frequency: ${FREQ:-N/A}'
-echo 'Speed: ${RATE:-N/A} Mb/s'
+echo 'ip: ${IP:-n/a}'
+echo 'gateway: ${GW:-n/a}'
+echo 'signal: ${SIGNAL:-n/a} %'
+echo 'frequency: ${FREQ:-n/a} mhz'
+echo 'speed: ${RATE:-n/a} mb/s'
 echo
-read -n 1 -s -p 'Press any key to close...'
+read -n 1 -s -p 'press any key to close...'
 "
 }
 
 wifi_enable() {
-  nmcli radio wifi on && notify-send "wifi enabled"
+  nmcli radio wifi on && notify-send "󰖩 wifi enabled"
 }
 
 wifi_disable() {
-  nmcli radio wifi off && notify-send "wifi disabled"
+  nmcli radio wifi off && notify-send "󰖪 wifi disabled"
 }
 
 # ===== GENERATE WIFI MENU =====
@@ -74,24 +67,23 @@ generate_wifi_list() {
         icon="󰤢"
       elif ((SIGNAL >= 25)); then
         icon="󰤥"
-      else icon="󰤨"; fi
+      else
+        icon="󰤨"
+      fi
       menu_connected+=("$icon  $SSID")
     elif echo "$saved" | grep -qx "$SSID"; then
-      # Saved but not connected
       icon="󰸋"
       menu_saved+=("$icon  $SSID")
     else
-      # New networks
       [[ "$SECURITY" == "--" ]] && icon="" || icon=""
       menu_new+=("$icon  $SSID")
     fi
   done < <(nmcli -t -f SIGNAL,SSID,SECURITY device wifi list ifname "$WIFI_IFACE")
 
-  # Combine menus in desired order
   WIFI_DISPLAY_MENU=("${menu_connected[@]}" "${menu_saved[@]}" "${menu_new[@]}")
   WIFI_PLAIN_MENU=()
   for item in "${WIFI_DISPLAY_MENU[@]}"; do
-    WIFI_PLAIN_MENU+=("${item:3}") # strip icon + space for internal use
+    WIFI_PLAIN_MENU+=("${item:3}") # strip icon + space
   done
 }
 
@@ -111,13 +103,13 @@ handle_saved_connection() {
       rofi -dmenu -i -p "$SSID") || break
 
     case "$choice" in
-    "  disconnect") nmcli connection down "$SSID" && notify-send "disconnected to wifi" "$SSID" ;;
-    "󱘖  connect") nmcli connection up id "$SSID" && notify-send "connected to wifi" "$SSID" ;;
+    "  disconnect") nmcli connection down "$SSID" && notify-send " disconnected to wifi" "$SSID" ;;
+    "󱘖  connect") nmcli connection up id "$SSID" && notify-send "󱘖 connected to wifi" "$SSID" ;;
     "󰑖  enable autoconnect") nmcli connection modify "$SSID" connection.autoconnect yes ;;
     "󰑗  disable autoconnect") nmcli connection modify "$SSID" connection.autoconnect no ;;
     "󰆴  forget")
       nmcli connection delete "$SSID"
-      notify-send "connected to wifi" "$SSID"
+      notify-send "󰆴 wifi network removed" "$SSID"
       break
       ;;
     esac
@@ -127,9 +119,42 @@ handle_saved_connection() {
 handle_new_connection() {
   local SSID="$1"
   local pwd
-  pwd=$(rofi -dmenu -p "Password for $SSID:") || return
-  [[ -z "$pwd" ]] && return
-  nmcli device wifi connect "$SSID" password "$pwd"
+  local from_secret=0
+  local store_choice
+
+  # Try to get password from secret-tool (KeePassXC Secret Service)
+  if pwd=$(secret-tool lookup ssid "$SSID" 2>/dev/null); then
+    from_secret=1
+  else
+    pwd=""
+  fi
+
+  while true; do
+    # Prompt if no password yet
+    if [[ -z "$pwd" ]]; then
+      pwd=$(rofi -dmenu -password -p "$SSID password:" 2>/dev/null) || return
+      [[ -z "$pwd" ]] && return
+      from_secret=0
+    fi
+
+    # Try to connect with nmcli
+    if nmcli device wifi connect "$SSID" password "$pwd"; then
+      notify-send "󰤟 Wi-Fi connected" "$SSID"
+
+      # Offer to store password if it was entered manually
+      if [[ $from_secret -eq 0 ]]; then
+        store_choice=$(printf "Yes\nNo" | rofi -dmenu -p "Store password for $SSID?" 2>/dev/null)
+        if [[ $store_choice == "Yes" ]]; then
+          echo -n "$pwd" | secret-tool store --label="$SSID" ssid "$SSID" UserName ""
+          notify-send "󰍡 Wi-Fi password stored" "$SSID"
+        fi
+      fi
+      return 0
+    else
+      notify-send "󰤨 Wi-Fi connection failed" "$SSID"
+      pwd="" # clear so we re-prompt
+    fi
+  done
 }
 
 handle_connection() {
@@ -148,34 +173,28 @@ main() {
   CONNECTED_SSID=$(nmcli -t -f ACTIVE,SSID dev wifi | awk -F: '$1=="yes"{print $2}')
   WIFI_STATUS=$(nmcli radio wifi)
 
-  # Only show Wi-Fi info if connected
   [[ -n "$CONNECTED_SSID" ]] && MENU+=("$OPT_SHOW_WIFI_INFO")
 
-  # Wi-Fi toggle
   if [[ "$WIFI_STATUS" == "disabled" ]]; then
     MENU+=("$OPT_WIFI_ENABLE")
   else
     MENU+=("$OPT_WIFI_DISABLE")
   fi
 
-  # Add separator and Wi-Fi list if enabled
   if [[ "$WIFI_STATUS" == "enabled" ]]; then
     MENU+=("$SEPARATOR")
     generate_wifi_list
     MENU+=("${WIFI_DISPLAY_MENU[@]}")
   fi
 
-  # Show menu
   local CHOICE
-  CHOICE=$(printf "%s\n" "${MENU[@]}" | rofi -dmenu -i -p "Wi-Fi Menu") || exit
+  CHOICE=$(printf "%s\n" "${MENU[@]}" | rofi -dmenu -i -p "wifi menu") || exit
 
   case "$CHOICE" in
   "$OPT_SHOW_WIFI_INFO") show_wifi_info ;;
   "$OPT_WIFI_ENABLE") wifi_enable ;;
   "$OPT_WIFI_DISABLE") wifi_disable ;;
-  "$SEPARATOR") ;; # do nothing for separator
-  # "$OPT_HOTSPOT_ENABLE") hotspot_enable ;;
-  # "$OPT_HOTSPOT_DISABLE") hotspot_disable ;;
+  "$SEPARATOR") ;; # do nothing
   *) handle_connection "${WIFI_PLAIN_MENU[$(($(printf "%s\n" "${WIFI_DISPLAY_MENU[@]}" | grep -nx "$CHOICE" | cut -d: -f1) - 1))]}" ;;
   esac
 }
