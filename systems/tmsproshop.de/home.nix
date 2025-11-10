42ca3a21710fb85121a3291b9c9086ab2b4d31b9
cethien <borislaw.sotnikow@gmx.de>
2025-11-10 18:05:40 +0100

    work: added sshconf

--- systems/tmsproshop.de/home.nix
+++ systems/tmsproshop.de/home.nix
@@ -14,9 +14,7 @@ in {
   home.homeDirectory = "/home/${username}";
   home.stateVersion = stateVersion;
 
-  programs = let
-    gitServer = "git.tmspro.shop";
-  in {
+  programs = {
     bash.bashrcExtra = mkBefore ''
       if [ -f ~/.nix-profile/etc/profile.d/nix.sh ]; then
         source ~/.nix-profile/etc/profile.d/nix.sh
@@ -24,16 +22,34 @@ in {
     '';
 
     ssh.matchBlocksExtra = {
-      gitServer = {
-        host = gitServer;
-        hostname = gitServer;
-        port = 2222;
+      "pve-01" = {
+        host = "pve-01";
+        hostname = "10.180.80.250";
       };
-    };
+      "pve-02" = {
+        host = "pve-02";
+        hostname = "10.180.80.252";
+      };
 
-    git.urlExtra = {
-      "ssh://git@${gitServer}".insteadOf = "https://${gitServer}";
-      "git@${gitServer}:".insteadOf = "local:";
+      weclapp = {
+        host = "weclapp-prod";
+        hostname = "10.180.80.97";
+      };
+
+      weclapp-test = {
+        host = "weclapp-test";
+        hostname = "10.180.80.94";
+      };
+
+      "docker-node-01" = {
+        host = "srv-admin-docker-cluster-node-01";
+        hostname = "10.180.80.101";
+      };
+
+      "docker-prod" = {
+        host = "docker-prod-1";
+        hostname = "10.180.80.93";
+      };
     };
   };
 }
