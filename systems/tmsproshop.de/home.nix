078244e2362ea5f449fd7e0354a7fb4332da58e3
cethien <borislaw.sotnikow@gmx.de>
2025-11-13 13:44:53 +0100

    tms: firefox and thunderbird for testing, ssh stuff

--- systems/tmsproshop.de/home.nix
+++ systems/tmsproshop.de/home.nix
@@ -15,6 +15,10 @@ in {
   home.stateVersion = stateVersion;
 
   programs = {
+    firefox.enable = true;
+    thunderbird.enable = true;
+    thunderbird.profiles."b.sotnikow@tmspro.shop".isDefault = true;
+
     bash.bashrcExtra = mkBefore ''
       if [ -f ~/.nix-profile/etc/profile.d/nix.sh ]; then
         source ~/.nix-profile/etc/profile.d/nix.sh
@@ -22,34 +26,20 @@ in {
     '';
 
     ssh.matchBlocksExtra = {
-      "pve-01" = {
-        host = "pve-01";
-        hostname = "10.180.80.250";
-      };
-      "pve-02" = {
-        host = "pve-02";
-        hostname = "10.180.80.252";
-      };
+      "*".identityFile = "~/.ssh/id_ed25519";
+      "*".user = "bso";
 
-      weclapp = {
-        host = "weclapp-prod";
-        hostname = "10.180.80.97";
-      };
+      "pve-01".hostname = "10.180.80.250";
+      "pve-02".hostname = "10.180.80.252";
+      "srv-admin-docker-cluster-node-01".hostname = "10.180.80.101";
 
-      weclapp-test = {
-        host = "weclapp-test";
-        hostname = "10.180.80.94";
-      };
+      "srv-weclapp-prod".hostname = "10.180.80.97";
+      "srv-weclapp-test".hostname = "10.180.80.94";
+      "srv-docker-prod-1".hostname = "10.180.80.93";
 
-      "docker-node-01" = {
-        host = "srv-admin-docker-cluster-node-01";
-        hostname = "10.180.80.101";
-      };
-
-      "docker-prod" = {
-        host = "docker-prod-1";
-        hostname = "10.180.80.93";
-      };
+      "srv-magento-content".hostname = "10.102.99.215";
+      "lxc-magento-runner".hostname = "10.180.80.202";
+      "lxc-magento-runner".user = "root";
     };
   };
 }
