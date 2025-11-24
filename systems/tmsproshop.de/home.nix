9f438b3b89f26a6477ad65c421d2bb98283ebae6
cethien <borislaw.sotnikow@gmx.de>
2025-11-24 10:27:55 +0100

    work: ssh-hosts

--- systems/tmsproshop.de/home.nix
+++ systems/tmsproshop.de/home.nix
@@ -29,17 +29,56 @@ in {
       "*".identityFile = "~/.ssh/id_ed25519";
       "*".user = "bso";
 
-      "pve-01".hostname = "10.180.80.250";
-      "pve-02".hostname = "10.180.80.252";
-      "srv-admin-docker-cluster-node-01".hostname = "10.180.80.101";
+      "srv-bitbucket-runner".hostname = "10.180.80.220";
+      "srv-magento-content".hostname = "10.102.99.215";
 
-      "srv-weclapp-prod".hostname = "10.180.80.97";
+      "lxc-wp-test-1" = {
+        user = "sysadmin";
+        hostname = "10.180.80.92";
+      };
+      "lxc-anna-auftrag" = {
+        user = "sysadmin";
+        hostname = "10.102.99.85";
+      };
+
+      "lxc-sdg" = {
+        user = "sysadmin";
+        hostname = "10.102.99.92";
+      };
+      "lxc-phone-search" = {
+        user = "sysadmin";
+        hostname = "10.102.99.91";
+      };
+      "lxc-toja-prod" = {
+        user = "sysadmin";
+        hostname = "10.102.99.205";
+      };
+      "lxc-sherlock-prod" = {
+        user = "sysadmin";
+        hostname = "10.102.99.90";
+      };
+      "srv-bnl" = {
+        user = "root";
+        hostname = "10.102.99.93";
+      };
+      "srv-weclapp-prod" = {
+        user = "root";
+        hostname = "10.180.80.97";
+      };
       "srv-weclapp-test".hostname = "10.180.80.94";
       "srv-docker-prod-1".hostname = "10.180.80.93";
+      "srv-admin-docker-cluster-node-01".hostname = "10.180.80.201";
+      "pve-01".hostname = "10.180.80.252";
+      "pve-02".hostname = "10.180.80.250";
 
-      "srv-magento-content".hostname = "10.102.99.215";
-      "lxc-magento-runner".hostname = "10.180.80.202";
-      "lxc-magento-runner".user = "root";
+      "maxcluster--srv-prod" = {
+        user = "web-user";
+        hostname = "109.71.72.118";
+      };
+      "maxcluster--srv-staging" = {
+        user = "web-user";
+        hostname = "109.71.72.244";
+      };
     };
   };
 }
