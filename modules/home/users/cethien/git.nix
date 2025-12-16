d26b0343f096af5d4db74e70e41b283059a15f18
cethien <borislaw.sotnikow@gmx.de>
2025-12-16 13:30:56 +0100

    home: user-specific stuff

--- modules/home/users/cethien/git.nix
+++ modules/home/users/cethien/git.nix
@@ -1,6 +1,8 @@
 {
-  config.programs.git.settings.user = {
-    name = "cethien";
-    email = "borislaw.sotnikow@gmx.de";
+  config = {
+    programs.git.settings.user = {
+      name = "cethien";
+      email = "borislaw.sotnikow@gmx.de";
+    };
   };
 }
--- modules/home/users/cethien/rclone.nix
+++ modules/home/users/cethien/rclone.nix
@@ -7,31 +7,25 @@
 in {
   config = mkIf config.programs.rclone.enable {
     sops.secrets."rclone/drive/token" = {};
-    programs.rclone = {
-      remotes = {
-        "gdrive" = {
-          mounts = {
-            "" = {
-              enable = true;
-              mountPoint = "${config.home.homeDirectory}/mnt/GDrive";
-              options = {
-                dir-cache-time = "5000h";
-                poll-interval = "10s";
-                umask = "002";
-                user-agent = "desktop";
-              };
-            };
-          };
-
-          config = {
-            type = "drive";
-            scope = "drive";
-          };
-          secrets = {
-            token = config.sops.secrets."rclone/drive/token".path;
+    programs.rclone.remotes."gdrive" = {
+      mounts = {
+        "" = {
+          enable = true;
+          mountPoint = "${config.home.homeDirectory}/GDrive";
+          options = {
+            dir-cache-time = "5000h";
+            poll-interval = "10s";
+            umask = "002";
+            user-agent = "desktop";
           };
         };
       };
+
+      config = {
+        type = "drive";
+        scope = "drive";
+      };
+      secrets.token = config.sops.secrets."rclone/drive/token".path;
     };
   };
 }
