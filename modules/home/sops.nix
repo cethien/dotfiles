949389fab04089a2dee43d388a39348fe8571e1b
cethien <borislaw.sotnikow@gmx.de>
2025-12-18 12:47:04 +0100

    home: sops fix

--- modules/home/sops.nix
+++ modules/home/sops.nix
@@ -9,7 +9,7 @@
 
   sops = {
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
-    defaultSopsFile = ./secrets/secrets.yaml;
+    defaultSopsFile = ./users/cethien/secrets.yaml;
     defaultSopsFormat = "yaml";
   };
 }
