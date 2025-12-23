6bb1fed9d7c23fb139a6f32625877b3492a265e5
cethien <borislaw.sotnikow@gmx.de>
2025-12-23 11:28:00 +0100

    home: keepass idle lock

--- modules/home/keepassxc.nix
+++ modules/home/keepassxc.nix
@@ -7,80 +7,82 @@
   inherit (lib) mkIf elem;
   hypr = elem "keepassxc" config.wayland.windowManager.hyprland.autostart;
 in {
-  config = let
-  in
-    mkIf config.programs.keepassxc.enable {
-      programs.keepassxc.settings = {
-        Browser = {
-          Enabled = true;
-          AlwaysAllowAccess = true;
-          CustomProxyLocation = "";
-        };
-
-        FdoSecrets = {
-          Enabled = true;
-          ConfirmAccessItem = false;
-          ConfirmDeleteItem = false;
-          ShowNotification = false;
-        };
-
-        SSHAgent.Enabled = true;
-
-        PasswordGenerator = {
-          Length = 24;
-          SpecialChars = false;
-        };
-
-        GUI = {
-          AdvancedSettings = true;
-          MinimizeOnClose = true;
-          MinimizeOnStartup = true;
-          MinimizeOnTray = true;
-          ShowTrayIcon = true;
-          # FontSizeOffset = 1;
-
-          ApplicationTheme = "dark";
-          HidePasswords = true;
-        };
-
-        General.MinimizeAfterUnlock = false;
-
-        Security.IconDownloadFallback = true;
+  config = mkIf config.programs.keepassxc.enable {
+    programs.keepassxc.settings = {
+      Browser = {
+        Enabled = true;
+        AlwaysAllowAccess = true;
+        CustomProxyLocation = "";
       };
 
-      services.ssh-agent.enable = true;
-      home.packages = with pkgs; [
-        libsecret
-      ];
-
-      home.sessionVariables.SUDO_ASKPASS = "${pkgs.writeShellScriptBin "sudo-askpass" ''
-        #!/usr/bin/env bash
-        secret-tool lookup id "$(whoami)@$(hostname)" sudo yes 2>/dev/null | head -n1
-      ''}/bin/sudo-askpass";
-
-      home.shellAliases.sudo = "${pkgs.writeShellScriptBin "sudo-wrapper" ''
-        #!/usr/bin/env bash
-        command sudo -A "$@" 2>/dev/null || command sudo "$@"
-      ''}/bin/sudo-wrapper";
-
-      services.syncthing.settings = mkIf config.services.syncthing.enable {
-        folders.keepass = {
-          id = "keepass";
-          path = "${config.home.homeDirectory}/.keepass";
-          devices = ["hp-430-g7" "xiaomi-15"];
-        };
+      FdoSecrets = {
+        Enabled = true;
+        ConfirmAccessItem = false;
+        ConfirmDeleteItem = false;
+        ShowNotification = false;
       };
 
-      wayland.windowManager.hyprland.settings = {
-        exec-once = mkIf hypr [
-          "keepassxc"
-        ];
+      SSHAgent.Enabled = true;
 
-        bind = [
-          "SUPER SHIFT, K, exec, ${
-            (pkgs.cethien.writeHyprlandLaunchScriptBin "keepassxc" "keepassxc" "org.keepassxc.KeePassXC").bin
-          }"
-        ];
+      PasswordGenerator = {
+        Length = 24;
+        SpecialChars = false;
+      };
+
+      GUI = {
+        AdvancedSettings = true;
+        MinimizeOnClose = true;
+        MinimizeOnStartup = true;
+        MinimizeOnTray = true;
+        ShowTrayIcon = true;
+        # FontSizeOffset = 1;
+
+        ApplicationTheme = "dark";
+        HidePasswords = true;
+      };
+
+      General.MinimizeAfterUnlock = false;
+
+      Security = {
+        IconDownloadFallback = true;
+        QuickUnlock = false;
+        LockDatabaseIdle = false;
       };
     };
+
+    services.ssh-agent.enable = true;
+    home.packages = with pkgs; [
+      libsecret
+    ];
+
+    home.sessionVariables.SUDO_ASKPASS = "${pkgs.writeShellScriptBin "sudo-askpass" ''
+      #!/usr/bin/env bash
+      secret-tool lookup id "$(whoami)@$(hostname)" sudo yes 2>/dev/null | head -n1
+    ''}/bin/sudo-askpass";
+
+    home.shellAliases.sudo = "${pkgs.writeShellScriptBin "sudo-wrapper" ''
+      #!/usr/bin/env bash
+      command sudo -A "$@" 2>/dev/null || command sudo "$@"
+    ''}/bin/sudo-wrapper";
+
+    services.syncthing.settings = mkIf config.services.syncthing.enable {
+      folders.keepass = {
+        id = "keepass";
+        path = "${config.home.homeDirectory}/.keepass";
+        devices = ["hp-430-g7" "xiaomi-15"];
+      };
+    };
+
+    wayland.windowManager.hyprland.settings = {
+      exec-once = mkIf hypr [
+        "keepassxc"
+      ];
+
+      bind = [
+        "SUPER SHIFT, K, exec, ${
+          (pkgs.cethien.writeHyprlandLaunchScriptBin "keepassxc" "keepassxc" "org.keepassxc.KeePassXC").bin
+        }"
+      ];
+    };
+  };
 }
