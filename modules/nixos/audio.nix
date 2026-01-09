279169e077b8cb92a8dfed3d68b39373f775ffec
cethien <borislaw.sotnikow@gmx.de>
2026-01-09 17:24:04 +0100

    nixos: anc

--- modules/nixos/audio.nix
+++ modules/nixos/audio.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   inherit (lib) mkIf;
@@ -10,11 +11,71 @@ in {
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
-      wireplumber.enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
+
+      wireplumber.enable = true;
+
+      extraConfig.pipewire."99-input-denoising" = let
+        mic = "alsa_input.usb-3142_Fifine_Microphone-00.mono-fallback";
+      in {
+        "context.modules" = [
+          {
+            name = "libpipewire-module-filter-chain";
+            args = {
+              "node.description" = "ANC (RNNoise)";
+              "media.name" = "ANC (RNNoise)";
+              "filter.graph" = {
+                nodes = [
+                  {
+                    type = "ladspa";
+                    name = "rnnoise";
+                    plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                    label = "noise_suppressor_mono";
+                    control = {"VAD Threshold (%)" = 70.0;};
+                  }
+                ];
+              };
+              "capture.props" = {
+                "node.passive" = true;
+                "node.target" = mic;
+              };
+              "playback.props" = {
+                "media.class" = "Audio/Source";
+                "node.name" = "rnnoise_source";
+              };
+            };
+          }
+          # --- DeepFilterNet Source ---
+          {
+            name = "libpipewire-module-filter-chain";
+            args = {
+              "node.description" = "ANC (DeepFilter)";
+              "media.name" = "ANC (DeepFilter)";
+              "filter.graph" = {
+                nodes = [
+                  {
+                    type = "ladspa";
+                    name = "deepfilter";
+                    plugin = "${pkgs.deepfilternet}/lib/ladspa/libdeep_filter_ladspa.so";
+                    label = "deep_filter_mono";
+                  }
+                ];
+              };
+              "capture.props" = {
+                "node.passive" = true;
+                "node.target" = mic;
+              };
+              "playback.props" = {
+                "media.class" = "Audio/Source";
+                "node.name" = "deepfilter_source";
+              };
+            };
+          }
+        ];
+      };
     };
   };
 }
