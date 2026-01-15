ad837de1bbba2205c9c57396f315b47f83f431e5
cethien <borislaw.sotnikow@gmx.de>
2026-01-15 18:11:02 +0100

    tmsproshop: added some more conns

--- homes/tmsproshop.de/rdp.nix
+++ homes/tmsproshop.de/rdp.nix
@@ -1,19 +1,24 @@
 let
   shared = {
     username = "Administrator";
-    domain = "ad.tmsproshop.de";
+    domain = "tmspro.shop";
     authenticationLevel = 2;
     dynamicResolution = true;
   };
+  old.domain = "ad.tmsproshop.de";
 in {
-  "hyper-v" = shared // {fullAddress = "10.102.99.89";};
-  "exchange" = shared // {fullAddress = "10.102.99.98";};
-  "ad_dc_dns" = shared // {fullAddress = "10.102.99.99";};
+  "dc-02" = shared // {fullAddress = "10.0.50.12";};
+  "dc-01" = shared // {fullAddress = "10.0.50.11";};
+  "exchange" = shared // {fullAddress = "10.0.50.10";};
   "timas" =
     shared
     // {
       fullAddress = "10.102.99.80";
-      domain = "";
       username = "LocalAdmin";
+      domain = null;
     };
+
+  "hyper-v" = shared // old // {fullAddress = "10.102.99.89";};
+  "exchange-2019" = shared // old // {fullAddress = "10.102.99.98";};
+  "dc-2016" = shared // old // {fullAddress = "10.102.99.99";};
 }
