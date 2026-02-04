63e4bca4cf76578555e0898d8643a7169a96f432
cethien <borislaw.sotnikow@gmx.de>
2026-02-04 20:28:44 +0100

    home: kitty tmux start on specific workspace

--- modules/home/terminals.nix
+++ modules/home/terminals.nix
@@ -1,4 +1,21 @@
 {
+  config,
+  pkgs,
+  ...
+}: let
+  script = pkgs.writeShellScriptBin "tmux-smart-attach" ''
+    if [ "$(hyprctl activeworkspace -j | jq '.id')" -eq 1 ] && command -v tmux >/dev/null; then
+      LAST_SESSION=$(tmux list-sessions -F "#{session_last_attached} #{session_name}" 2>/dev/null | sort -nr | head -n1 | awk '{print $2}')
+      if [ -n "$LAST_SESSION" ]; then
+        exec kitty --title "tmux-main" tmux attach-session -t "$LAST_SESSION"
+      else
+        exec kitty --title "tmux-main" tmux new-session -A -s main
+      fi
+    else
+      exec kitty
+    fi
+  '';
+in {
   programs.kitty = {
     settings = {
       window_padding_width = 2;
@@ -12,7 +29,10 @@
   };
 
   wayland.windowManager.hyprland.settings = {
-    "$terminal" = "kitty";
+    "$terminal" =
+      if config.programs.tmux.enable
+      then "${script}/bin/tmux-smart-attach"
+      else "kitty";
 
     bind = [
       "SUPER, RETURN, exec, $terminal"
