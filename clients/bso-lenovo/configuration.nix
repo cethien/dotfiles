2f39611c450e982fc2b8805b44896c375b3a8580
cethien <borislaw.sotnikow@gmx.de>
2026-02-13 13:47:55 +0100

    add: client bso-lenovo

---
+++ clients/bso-lenovo/configuration.nix
@@ -0,0 +1,62 @@
+{pkgs, ...}: {
+  imports = [
+    ../../modules/nixos
+  ];
+
+  # services.pipewire.active-mic = "alsa_input.usb-3142_Fifine_Microphone-00.mono-fallback";
+
+  services.printing.enable = true;
+  hardware = {
+    enableRedistributableFirmware = true;
+    enableAllFirmware = true;
+
+    bluetooth.enable = true;
+
+    # scanner
+    sane.enable = true;
+  };
+
+  virtualisation = {
+    docker.enable = true;
+    libvirtd.enable = true;
+  };
+
+  deeznuts = {
+    desktop = {
+      autologinUser = "bsotnikow";
+      hyprland.enable = true;
+    };
+    net-tools.enable = true;
+  };
+
+  networking.hostName = "bso-lenovo";
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    plymouth = {
+      enable = true;
+      theme = "colorful_sliced";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = ["colorful_sliced"];
+        })
+      ];
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+  };
+}
