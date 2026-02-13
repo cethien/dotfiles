764f194ffafd4025ce4b8673919143fe35d4c30d
cethien <borislaw.sotnikow@gmx.de>
2026-02-13 13:47:55 +0100

    add: client bso-lenovo

---
+++ clients/bso-lenovo/home.nix
@@ -0,0 +1,49 @@
+{pkgs, ...}: {
+  imports = [
+    ../../modules/home
+    ../../modules/home/users/cethien
+  ];
+
+  stylix.image = ../../wallpapers/bliss-windows-night-nologo-8k-unofficial.png;
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      monitor = [
+        "DP-1, 1920x1080@60, 0x0, 1"
+        "eDP-1, 1920x1080@60, 340x1440, 1"
+      ];
+      general.allow_tearing = true;
+
+      workspace = [
+        "1, monitor:DP-1, persistent:true"
+        "2, monitor:DP-1, persistent:true"
+        "3, monitor:DP-1, persistent:true"
+
+        "4, monitor:eDP-1, persistent:true"
+        "5, monitor:eDP-1, persistent:true"
+        "6, monitor:eDP-1, persistent:true"
+      ];
+    };
+
+    autostart = [
+      "keepassxc"
+      "spotify"
+    ];
+  };
+  programs.hyprlock.monitor = "DP-1";
+
+  home.packages = with pkgs; [simple-scan libreoffice];
+
+  programs = {
+    ssh.matchBlocksExtra = import ../../homes/bso-lenovo/ssh.nix;
+
+    kitty.enable = true;
+    chromium.enable = true;
+    keepassxc.enable = true;
+    gemini-cli.enable = true;
+
+    devSuite.extras = ["containers"];
+
+    spotify.enable = true;
+  };
+}
--- modules/nixos/users/cethien.nix
+++ modules/nixos/users/cethien.nix
@@ -21,4 +21,18 @@ in {
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKgrZmsUHZn7BAGAl83RUkNejlhJbmLr3lklrlVzy2Zz borislaw.sotnikow@gmx.de"
     ];
   };
+
+  users.users.bsotnikow = {
+    enable = mkDefault false;
+    isNormalUser = true;
+    extraGroups = mkMerge [
+      ["networkmanager" "wheel"]
+      (mkIf config.deeznuts.net-tools.enable ["nettools"])
+      (mkIf config.hardware.sane.enable ["scanner"])
+      (mkIf config.hardware.uinput.enable ["uinput" "input"])
+      (mkIf config.virtualisation.docker.enable ["docker"])
+      (mkIf config.virtualisation.libvirtd.enable ["libvirtd" "kvm"])
+    ];
+    hashedPassword = "$y$j9T$OqozgiTqzcpfQ9zbKkhvr0$i4lBjUVMF1xpGCOm57m/jXVWt0KVDh/evHHFq9mF7Z4";
+  };
 }
