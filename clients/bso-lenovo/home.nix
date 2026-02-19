7933a9f6dfb0070d5fff079ed12a31ce5b6a54e6
cethien <borislaw.sotnikow@gmx.de>
2026-02-19 16:53:29 +0100

    chore: monitor setup

--- clients/bso-lenovo/home.nix
+++ clients/bso-lenovo/home.nix
@@ -1,4 +1,11 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: let
+  monIn = "eDP-1";
+  monEx = "desc:Eizo Nanao Corporation EV2430 33096078";
+in {
   imports = [
     ../../modules/home
     ../../modules/home/users/cethien
@@ -10,28 +17,34 @@
     enable = true;
     settings = {
       monitor = [
-        "DP-1, 1920x1080@60, 0x0, 1"
-        "eDP-1, 1920x1080@60, 340x1440, 1"
+        "${monEx}, 1920x1200@60, 0x0, 1"
+        "${monIn}, 1920x1080@60, 480x1200, 1"
       ];
       general.allow_tearing = true;
 
       exec-once = ["[silent] slack -u"];
-      workspace = [
-        "1, monitor:DP-1, persistent:true"
-        "2, monitor:DP-1, persistent:true"
-        "3, monitor:DP-1, persistent:true"
 
-        "4, monitor:eDP-1, persistent:true"
-        "5, monitor:eDP-1, persistent:true"
-        "6, monitor:eDP-1, persistent:true"
+      workspace = [
+        "1, monitor:${monEx}, persistent:true"
+        "2, monitor:${monEx}, persistent:true"
+        "3, monitor:${monEx}, persistent:true"
+
+        "4, monitor:${monIn}, persistent:true"
+        "5, monitor:${monIn}, persistent:true"
+        "6, monitor:${monIn}, persistent:true"
       ];
     };
 
     autostart = [
       "keepassxc"
+      "zen"
     ];
+    defaultWorkspaces = {
+      browser = 4;
+    };
   };
-  programs.hyprlock.monitor = "DP-1";
+  programs.hyprlock.monitor = "${monIn}";
+
   home.packages = with pkgs; [
     simple-scan
     libreoffice
