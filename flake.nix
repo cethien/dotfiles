eca9174df4ad2cebb8accea509ba3dcca71e3699
cethien <borislaw.sotnikow@gmx.de>
2026-02-23 14:33:40 +0100

    chore: flake client home cleanup

--- flake.nix
+++ flake.nix
@@ -169,7 +169,8 @@
 
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
+    # home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:cethien/home-manager/rclone/smb-sanitizing";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     stylix.url = "github:nix-community/stylix";
