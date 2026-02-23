c17025782a9d01c56b80c49c2bddd33877bae5b0
cethien <borislaw.sotnikow@gmx.de>
2026-02-23 14:31:18 +0100

    chore(tms): bookmark + extensions
    and some fixes

--- homes/tms-bso/zen-browser.nix
+++ homes/tms-bso/zen-browser.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   config = {
     programs.zen-browser.enable = true;
     programs.browser.default = config.programs.zen-browser.package;
@@ -14,7 +18,7 @@
           color = "purple";
           icon = "circle";
         };
-        "tailscale" = {
+        "tailscale/member" = {
           id = 3;
           color = "yellow";
           icon = "vacation";
@@ -27,7 +31,7 @@
       };
 
       spaces."lets get this bread" = {
-        id = "cd0b7a9b-bb11-42e8-a10a-52ea6813e6b4";
+        id = "8e725798-defd-4b13-bc8b-ae3fd4bad512";
         position = 1000;
         icon = "🍞";
       };
@@ -41,34 +45,102 @@
         };
         "tmspro.shop" = {
           id = "96070d37-4b78-490f-87d1-884474bd9434";
-          url = "https://start.tmspro.shop:7575";
+          url = "http://start.tmspro.shop:7575";
           isEssential = true;
           position = 102;
         };
-
         "Outlook" = {
           id = "eee232e4-72d9-4d7c-b4e6-02a1f6155f7f";
           url = "https://outlook.tmsproshop.de/owa";
           isEssential = true;
           position = 110;
         };
-        "Jira" = {
-          id = "8af62707-0722-4049-9801-bedced343333";
-          url = "https://tmsproshop.atlassian.net/jira/for-you";
-          position = 111;
-        };
-        "Netgate pfSense Plus" = {
-          id = "fb316d70-2b5e-4c46-bf42-f4e82d635153";
-          url = "https://github.com/";
-          position = 112;
-        };
-        "proxmox" = {
-          id = "";
-          url = "https://10.180.80.252:8006";
-          position = 113;
-        };
       };
+
+      bookmarks = [
+        {
+          name = "passbolt";
+          url = "https://pass.tmspro.shop";
+          tags = ["passwords"];
+        }
+        {
+          name = "vaultwarden";
+          url = "https://vault.tmspro.shop";
+          tags = ["passwords"];
+        }
+        {
+          name = "bookstack";
+          url = "https://docs.tmspro.shop";
+          tags = ["docs"];
+        }
+
+        {
+          name = "jira";
+          url = "https://tmsproshop.atlassian.net/jira";
+          tags = ["atlassian" "todo"];
+        }
+        {
+          name = "confluence";
+          url = "https://tmsproshop.atlassian.net/wiki";
+          tags = ["atlassian" "docs"];
+        }
+        {
+          name = "allinkl";
+          url = "https://kasserver.com/";
+          tags = ["kasserver" "email" "domains" "cdn"];
+        }
+        {
+          name = "tailscale admin";
+          url = "https://login.tailscale.com/admin";
+          tags = ["tailscale" "vpn"];
+        }
+
+        {
+          name = "pve-node-a";
+          url = "https://10.180.80.252:8006";
+          tags = ["proxmox"];
+        }
+        {
+          name = "pve-node-b";
+          url = "https://10.180.80.250:8006";
+          tags = ["proxmox"];
+        }
+        {
+          name = "pve-node-c";
+          url = "https://10.0.10.7:8006";
+          tags = ["proxmox"];
+        }
+        {
+          name = "pfsense";
+          url = "https://10.180.80.254";
+          tags = ["netgate" "firewall"];
+        }
+        {
+          name = "unifi";
+          url = "https://10.180.80.99";
+          tags = ["switch" "network" "ubiquity"];
+        }
+        {
+          name = "pve storage";
+          url = "https://10.180.80.3:5001/";
+          tags = ["synology" "nas" "nfs"];
+        }
+        {
+          name = "smb storage";
+          url = "https://10.102.99.213:5001/";
+          tags = ["synology" "san" "smb"];
+        }
+      ];
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        passbolt
+        bitwarden
+      ];
     in {
+      bookmarks = {
+        force = true;
+        settings = [{inherit bookmarks;}];
+      };
+      extensions.packages = extensions;
       containersForce = true;
       inherit containers;
       pinsForce = true;
