879dffbe76ef32d70da36ee37fbc8ef7e614635e
cethien <borislaw.sotnikow@gmx.de>
2026-03-05 15:04:58 +0100

    chore: hyprland curor effec
    woooosh

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -37,223 +37,244 @@ in {
     services.mako.enable = true;
     programs.rofi.enable = true;
 
-    wayland.windowManager.hyprland.settings = {
-      exec-once = [
-        "${pkgs.udiskie}/bin/udiskie"
-        "${
-          pkgs.writeShellScriptBin "notify-battery" (builtins.readFile ./notify-battery.sh)
-        }/bin/notify-battery"
+    wayland.windowManager.hyprland = {
+      plugins = with pkgs.hyprlandPlugins; [
+        hypr-dynamic-cursors
       ];
 
-      monitor = mkDefault [
-        "eDP-1, 1920x1080@60, 0x0, 1"
-      ];
+      extraConfig = ''
+        plugin {
+          dynamic-cursors {
+            enabled = true
+            mode = stretch
+            threshhold = 1
+            stretch {
+                limit = 1500
+                function = linear
+            }
+            shake {
+              enabled = false
+            }
+          }
+        }
+      '';
 
-      workspace = mkDefault [
-        "1, monitor:eDP-1, persistent:true, default:true"
-        "2, monitor:eDP-1, persistent:true, default:false"
-        "3, monitor:eDP-1, persistent:true, default:false"
-      ];
-
-      xwayland = {
-        force_zero_scaling = true;
-      };
-
-      general = {
-        gaps_in = 2;
-        gaps_out = 4;
-        border_size = 4;
-        # "col.active_border" = "";
-        # "col.inactive_border" = "";
-
-        layout = "master";
-      };
-
-      master = {
-        mfact = 0.6;
-        orientation = "right";
-      };
-
-      dwindle = {
-        pseudotile = true;
-        preserve_split = true;
-        force_split = 1;
-      };
-
-      decoration = {
-        rounding = 6;
-
-        active_opacity = 1.0;
-        inactive_opacity = 0.975;
-        fullscreen_opacity = 1.0;
-
-        shadow = {
-          enabled = true;
-          range = 12;
-          render_power = 3;
-          # color = "$crust";
-        };
-
-        blur = {
-          enabled = true;
-          size = 6;
-          passes = 1;
-          vibrancy = 0.1696;
-          new_optimizations = true;
-        };
-      };
-
-      animations = {
-        enabled = true;
-
-        bezier = [
-          "deez, 0.10, 0.9, 0.1, 1.05"
+      settings = {
+        exec-once = [
+          "${pkgs.udiskie}/bin/udiskie"
+          "${
+            pkgs.writeShellScriptBin "notify-battery" (builtins.readFile ./notify-battery.sh)
+          }/bin/notify-battery"
         ];
 
-        animation = [
-          "windows, 1, 5, deez, slide"
-          "border, 1, 3, deez"
-          "fade, 1, 3, deez"
-          "workspaces, 1, 3, deez"
+        monitor = mkDefault [
+          "eDP-1, 1920x1080@60, 0x0, 1"
         ];
-      };
 
-      windowrule = [
-        "suppress_event maximize, match:class .*"
-        "no_focus on, match:class ^$, match:title ^$, match:xwayland 1, match:float 1, match:fullscreen 0, match:pin 0"
-      ];
+        workspace = mkDefault [
+          "1, monitor:eDP-1, persistent:true, default:true"
+          "2, monitor:eDP-1, persistent:true, default:false"
+          "3, monitor:eDP-1, persistent:true, default:false"
+        ];
 
-      misc = {
-        force_default_wallpaper = 0;
-        disable_hyprland_logo = true;
-        middle_click_paste = false;
-        focus_on_activate = true;
-        enable_swallow = true;
-      };
+        xwayland = {
+          force_zero_scaling = true;
+        };
 
-      input = {
-        kb_layout = "de";
-        kb_variant = "nodeadkeys";
-        follow_mouse = 2;
-      };
+        general = {
+          gaps_in = 2;
+          gaps_out = 4;
+          border_size = 4;
 
-      "$resizeIncrement" = 25;
+          layout = "master";
+        };
 
-      bind =
-        # let
-        # vol-app = pkgs.writeShellScriptBin "vol-app" ''
-        #   # 1. Aktive PID von Hyprland holen
-        #   ACTIVE_PID=$(hyprctl activewindow -j | ${pkgs.jq}/bin/jq '.pid')
-        #
-        #   if [ "$ACTIVE_PID" = "null" ] || [ -z "$ACTIVE_PID" ]; then exit 0; fi
-        #
-        #   # 2. Die PipeWire Node-ID finden, die zu dieser PID (oder deren Child-Prozessen) gehört
-        #   # Wir dumpen die Nodes und suchen nach dem process.id match
-        #   NODE_ID=$(${pkgs.pipewire}/bin/pw-dump | ${pkgs.jq}/bin/jq -r ".[] | select(.info.props[\"process.id\"] == $ACTIVE_PID or .info.props[\"application.process.id\"] == \"$ACTIVE_PID\") | .id" | head -n1)
-        #
-        #   # 3. Falls nichts gefunden (wegen Proton/Steam), suchen wir nach Child-PIDs
-        #   if [ -z "$NODE_ID" ] || [ "$NODE_ID" = "null" ]; then
-        #       CHILD_PIDS=$(pgrep -P "$ACTIVE_PID")
-        #       for CPID in $CHILD_PIDS; do
-        #           NODE_ID=$(${pkgs.pipewire}/bin/pw-dump | ${pkgs.jq}/bin/jq -r ".[] | select(.info.props[\"process.id\"] == $CPID or .info.props[\"application.process.id\"] == \"$CPID\") | .id" | head -n1)
-        #           [ -n "$NODE_ID" ] && [ "$NODE_ID" != "null" ] && break
-        #       done
-        #   fi
-        #
-        #   # 4. Lautstärke via wpctl anpassen, wenn eine Node gefunden wurde
-        #   if [ -n "$NODE_ID" ] && [ "$NODE_ID" != "null" ]; then
-        #       ${pkgs.wireplumber}/bin/wpctl set-volume "$NODE_ID" "$1"
-        #   else
-        #       # Optional: Falls kein App-Stream gefunden, mach halt Default
-        #       ${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ "$1"
-        #   fi
-        # '';
-        # in
-        [
-          # "CTRL ALT, XF86AudioRaiseVolume, exec, ${vol-app}/bin/vol-app 5%+"
-          # "CTRL ALT, XF86AudioLowerVolume, exec, ${vol-app}/bin/vol-app 5%-"
+        master = {
+          mfact = 0.6;
+          orientation = "right";
+        };
+
+        dwindle = {
+          pseudotile = true;
+          preserve_split = true;
+          force_split = 1;
+        };
+
+        decoration = {
+          rounding = 6;
+
+          active_opacity = 1.0;
+          inactive_opacity = 0.975;
+          fullscreen_opacity = 1.0;
+
+          shadow = {
+            enabled = true;
+            range = 12;
+            render_power = 3;
+            # color = "$crust";
+          };
+
+          blur = {
+            enabled = true;
+            size = 6;
+            passes = 1;
+            vibrancy = 0.1696;
+            new_optimizations = true;
+          };
+        };
+
+        animations = {
+          enabled = true;
+
+          bezier = [
+            "deez, 0.10, 0.9, 0.1, 1.05"
+          ];
+
+          animation = [
+            "windows, 1, 5, deez, slide"
+            "border, 1, 3, deez"
+            "fade, 1, 3, deez"
+            "workspaces, 1, 3, deez"
+          ];
+        };
+
+        windowrule = [
+          "suppress_event maximize, match:class .*"
+          "no_focus on, match:class ^$, match:title ^$, match:xwayland 1, match:float 1, match:fullscreen 0, match:pin 0"
+        ];
+
+        misc = {
+          force_default_wallpaper = 0;
+          disable_hyprland_logo = true;
+          middle_click_paste = false;
+          focus_on_activate = true;
+          enable_swallow = true;
+        };
+
+        input = {
+          kb_layout = "de";
+          kb_variant = "nodeadkeys";
+          follow_mouse = 2;
+        };
+
+        "$resizeIncrement" = 25;
+
+        bind =
+          # let
+          # vol-app = pkgs.writeShellScriptBin "vol-app" ''
+          #   # 1. Aktive PID von Hyprland holen
+          #   ACTIVE_PID=$(hyprctl activewindow -j | ${pkgs.jq}/bin/jq '.pid')
           #
-          "SUPER SHIFT, C, exec, ${pkgs.hyprpicker}/bin/hyprpicker -a"
+          #   if [ "$ACTIVE_PID" = "null" ] || [ -z "$ACTIVE_PID" ]; then exit 0; fi
+          #
+          #   # 2. Die PipeWire Node-ID finden, die zu dieser PID (oder deren Child-Prozessen) gehört
+          #   # Wir dumpen die Nodes und suchen nach dem process.id match
+          #   NODE_ID=$(${pkgs.pipewire}/bin/pw-dump | ${pkgs.jq}/bin/jq -r ".[] | select(.info.props[\"process.id\"] == $ACTIVE_PID or .info.props[\"application.process.id\"] == \"$ACTIVE_PID\") | .id" | head -n1)
+          #
+          #   # 3. Falls nichts gefunden (wegen Proton/Steam), suchen wir nach Child-PIDs
+          #   if [ -z "$NODE_ID" ] || [ "$NODE_ID" = "null" ]; then
+          #       CHILD_PIDS=$(pgrep -P "$ACTIVE_PID")
+          #       for CPID in $CHILD_PIDS; do
+          #           NODE_ID=$(${pkgs.pipewire}/bin/pw-dump | ${pkgs.jq}/bin/jq -r ".[] | select(.info.props[\"process.id\"] == $CPID or .info.props[\"application.process.id\"] == \"$CPID\") | .id" | head -n1)
+          #           [ -n "$NODE_ID" ] && [ "$NODE_ID" != "null" ] && break
+          #       done
+          #   fi
+          #
+          #   # 4. Lautstärke via wpctl anpassen, wenn eine Node gefunden wurde
+          #   if [ -n "$NODE_ID" ] && [ "$NODE_ID" != "null" ]; then
+          #       ${pkgs.wireplumber}/bin/wpctl set-volume "$NODE_ID" "$1"
+          #   else
+          #       # Optional: Falls kein App-Stream gefunden, mach halt Default
+          #       ${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ "$1"
+          #   fi
+          # '';
+          # in
+          [
+            # "CTRL ALT, XF86AudioRaiseVolume, exec, ${vol-app}/bin/vol-app 5%+"
+            # "CTRL ALT, XF86AudioLowerVolume, exec, ${vol-app}/bin/vol-app 5%-"
+            #
+            "SUPER SHIFT, C, exec, ${pkgs.hyprpicker}/bin/hyprpicker -a"
 
-          # "SUPER, M, exit"
+            # "SUPER, M, exit"
 
-          "ALT, F4, killactive"
-          "SUPER, C, killactive"
+            "ALT, F4, killactive"
+            "SUPER, C, killactive"
 
-          "SUPER, V, togglefloating"
-          "SUPER, P, pseudo"
-          "SUPER, J, togglesplit"
-          "SUPER, F, fullscreen"
+            "SUPER, V, togglefloating"
+            "SUPER, P, pseudo"
+            "SUPER, J, togglesplit"
+            "SUPER, F, fullscreen"
 
-          # move focus
-          "SUPER, left, movefocus, l"
-          "SUPER, right, movefocus, r"
-          "SUPER, up, movefocus, u"
-          "SUPER, down, movefocus, d"
+            # move focus
+            "SUPER, left, movefocus, l"
+            "SUPER, right, movefocus, r"
+            "SUPER, up, movefocus, u"
+            "SUPER, down, movefocus, d"
 
-          # move windows around inside a workspace
-          "SUPER SHIFT, left, movewindow, l"
-          "SUPER SHIFT, right, movewindow, r"
-          "SUPER SHIFT, up, movewindow, u"
-          "SUPER SHIFT, down, movewindow, d"
+            # move windows around inside a workspace
+            "SUPER SHIFT, left, movewindow, l"
+            "SUPER SHIFT, right, movewindow, r"
+            "SUPER SHIFT, up, movewindow, u"
+            "SUPER SHIFT, down, movewindow, d"
 
-          # scroll through existing workspaces
-          "SUPER CTRL, right, workspace, e+1"
-          "SUPER CTRL, left, workspace, e-1"
+            # scroll through existing workspaces
+            "SUPER CTRL, right, workspace, e+1"
+            "SUPER CTRL, left, workspace, e-1"
 
-          # navigate workspaces
-          "SUPER, 1, workspace, 1"
-          "SUPER, 2, workspace, 2"
-          "SUPER, 3, workspace, 3"
-          "SUPER, 4, workspace, 4"
-          "SUPER, 5, workspace, 5"
-          "SUPER, 6, workspace, 6"
-          "SUPER, 7, workspace, 7"
-          "SUPER, 8, workspace, 8"
-          "SUPER, 9, workspace, 9"
-          "SUPER, 0, workspace, 10"
+            # navigate workspaces
+            "SUPER, 1, workspace, 1"
+            "SUPER, 2, workspace, 2"
+            "SUPER, 3, workspace, 3"
+            "SUPER, 4, workspace, 4"
+            "SUPER, 5, workspace, 5"
+            "SUPER, 6, workspace, 6"
+            "SUPER, 7, workspace, 7"
+            "SUPER, 8, workspace, 8"
+            "SUPER, 9, workspace, 9"
+            "SUPER, 0, workspace, 10"
 
-          # move window to workspace
-          "SUPER CTRL SHIFT, right, movetoworkspace, e+1"
-          "SUPER CTRL SHIFT, left, movetoworkspace, e-1"
-          "SUPER CTRL SHIFT, 1, movetoworkspace, 1"
-          "SUPER CTRL SHIFT, 2, movetoworkspace, 2"
-          "SUPER CTRL SHIFT, 3, movetoworkspace, 3"
-          "SUPER CTRL SHIFT, 4, movetoworkspace, 4"
-          "SUPER CTRL SHIFT, 5, movetoworkspace, 5"
-          "SUPER CTRL SHIFT, 6, movetoworkspace, 6"
-          "SUPER CTRL SHIFT, 7, movetoworkspace, 7"
-          "SUPER CTRL SHIFT, 8, movetoworkspace, 8"
-          "SUPER CTRL SHIFT, 9, movetoworkspace, 9"
-          "SUPER CTRL SHIFT, 0, movetoworkspace, 10"
+            # move window to workspace
+            "SUPER CTRL SHIFT, right, movetoworkspace, e+1"
+            "SUPER CTRL SHIFT, left, movetoworkspace, e-1"
+            "SUPER CTRL SHIFT, 1, movetoworkspace, 1"
+            "SUPER CTRL SHIFT, 2, movetoworkspace, 2"
+            "SUPER CTRL SHIFT, 3, movetoworkspace, 3"
+            "SUPER CTRL SHIFT, 4, movetoworkspace, 4"
+            "SUPER CTRL SHIFT, 5, movetoworkspace, 5"
+            "SUPER CTRL SHIFT, 6, movetoworkspace, 6"
+            "SUPER CTRL SHIFT, 7, movetoworkspace, 7"
+            "SUPER CTRL SHIFT, 8, movetoworkspace, 8"
+            "SUPER CTRL SHIFT, 9, movetoworkspace, 9"
+            "SUPER CTRL SHIFT, 0, movetoworkspace, 10"
 
-          "SUPER, p, exec, ${
-            pkgs.writeShellScriptBin "notify-info" (builtins.readFile ./notify-info.sh)
-          }/bin/notify-info"
+            "SUPER, p, exec, ${
+              pkgs.writeShellScriptBin "notify-info" (builtins.readFile ./notify-info.sh)
+            }/bin/notify-info"
+          ];
+
+        binde = [
+          # resize windows
+          "SUPER ALT, right, resizeactive, $resizeIncrement 0"
+          "SUPER ALT, left, resizeactive, -$resizeIncrement 0"
+          "SUPER ALT, up, resizeactive, 0 -$resizeIncrement"
+          "SUPER ALT, down, resizeactive, 0 $resizeIncrement"
         ];
 
-      binde = [
-        # resize windows
-        "SUPER ALT, right, resizeactive, $resizeIncrement 0"
-        "SUPER ALT, left, resizeactive, -$resizeIncrement 0"
-        "SUPER ALT, up, resizeactive, 0 -$resizeIncrement"
-        "SUPER ALT, down, resizeactive, 0 $resizeIncrement"
-      ];
+        bindm = [
+          "SUPER, mouse:272, movewindow"
+          "SUPER, mouse:273, resizewindow"
+        ];
 
-      bindm = [
-        "SUPER, mouse:272, movewindow"
-        "SUPER, mouse:273, resizewindow"
-      ];
+        bindl = [
+          ", XF86MonBrightnessUp, exec, brightnessctl s 5%+"
+          ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
 
-      bindl = [
-        ", XF86MonBrightnessUp, exec, brightnessctl s 5%+"
-        ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
-
-        "SUPER, XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.2 @DEFAULT_AUDIO_SINK@ 5%+"
-        "SUPER, XF86AudioLowerVolume, exec, wpctl set-volume -l 1.2 @DEFAULT_AUDIO_SINK@ 5%-"
-        "SUPER, XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        "SUPER, XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-      ];
+          "SUPER, XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.2 @DEFAULT_AUDIO_SINK@ 5%+"
+          "SUPER, XF86AudioLowerVolume, exec, wpctl set-volume -l 1.2 @DEFAULT_AUDIO_SINK@ 5%-"
+          "SUPER, XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          "SUPER, XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ];
+      };
     };
   };
 }
