e81096ab2f91ba4bdc6248d4266f57716d6b7fb7
cethien <borislaw.sotnikow@gmx.de>
2026-03-10 17:49:11 +0100

    chore: git stuff

--- modules/home/dev/git.nix
+++ modules/home/dev/git.nix
@@ -26,9 +26,8 @@ in {
       };
 
       alias = {
-        ignore = "!gi() { ${pkgs.git-ignore}/bin/git-ignore $@ ;}; gi";
-        license = "!gl() { ${pkgs.license-go}/bin/license $@ ;}; gl";
-        stat = "!gscc() {${pkgs.scc}/bin/scc ;}; gscc";
+        ignore = "!gi() { ${pkgs.git-ignore}/bin/git-ignore -w $@;}; gi";
+        license = "!gl() { ${pkgs.license-go}/bin/license -o LICENSE $@ ;}; gl";
       };
       core = {
         eol = "lf";
@@ -59,7 +58,49 @@ in {
     programs.lazygit = {
       enable = config.programs.git.enable;
       shellWrapperName = "lzg";
-      settings.os.openDirInEditor = "$EDITOR";
+      settings = {
+        update.method = "never";
+        disableStartupPopups = true;
+        notARepository = "quit";
+        promptToReturnFromSubprocess = false;
+        os.openDirInEditor = "$EDITOR";
+
+        customCommands = [
+          {
+            key = "l";
+            description = "Add LICENSE file";
+            context = "files";
+            command = "${pkgs.license-go}/bin/license -o LICENSE {{.Form.License}}";
+            prompts = [
+              {
+                type = "menuFromCommand";
+                title = "Select License";
+                key = "License";
+                command = "${pkgs.license-go}/bin/license -list";
+                filter = "^(?P<id>\\S+)\\s+(?P<desc>.*)";
+                valueFormat = "{{ .id }}";
+                labelFormat = "{{ .id | yellow }} {{ .desc | cyan }}";
+              }
+            ];
+          }
+          {
+            key = "<f3>";
+            description = "open gh-dash";
+            command = "${pkgs.gh-dash}/bin/gh-dash";
+            context = "global";
+            output = "terminal";
+          }
+
+          {
+            key = "<f1>";
+            description = "Show code statistics";
+            command = "${pkgs.scc}/bin/scc --no-cocomo --no-size";
+            context = "global";
+            output = "popup";
+            outputTitle = "Stats";
+          }
+        ];
+      };
     };
     programs.tmux.resurrectPluginProcesses = ["lazygit"];
   };
