fa807fb945188a53fce5dfb06f736dc6b6b0e146
cethien <borislaw.sotnikow@gmx.de>
2026-03-20 15:33:56 +0100

    chore(mpv): settings and scripts
    fuckin weebs everywhere!!!

--- modules/home/desktop/hyprland/common-gui.nix
+++ modules/home/desktop/hyprland/common-gui.nix
@@ -26,7 +26,35 @@ in {
         config = {
           keep-open = true;
           ytdl-format = "bestvideo+bestaudio";
+          wayland-content-type = "none";
+          target-colorspace-hint = "no";
+
+          osc = "no";
+          osd-bar = "no";
+          border = "no";
         };
+        scripts = with pkgs.mpvScripts; [
+          # mpv-osc-modern
+          # modernx
+          uosc
+          evafast
+          thumbfast
+          dynamic-crop
+
+          youtube-upnext
+          sponsorblock-minimal
+          # sponsorblock
+          quality-menu
+
+          mpris
+          memo
+          cutter
+          easycrop
+          videoclip
+          mpv-webm
+          convert
+          chapterskip
+        ];
       };
       zathura.enable = true;
     };
