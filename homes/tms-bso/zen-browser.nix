e103ac30ac167026469a0b724e700643666fdae3
cethien <borislaw.sotnikow@gmx.de>
2026-03-20 14:49:13 +0100

    chore(tms/zen): bookmarks rework

--- homes/tms-bso/zen-browser.nix
+++ homes/tms-bso/zen-browser.nix
@@ -63,22 +63,92 @@
 
       bookmarks = [
         {
-          name = "o2business";
-          url = "https://admin.digitalphone.o2business.de/";
-          tags = ["telefonie" "phone"];
+          name = "startpage";
+          url = "https://tmspro.shop";
+          tags = ["landingpage"];
+        }
+        {
+          name = "vaultwarden";
+          url = "https://vault.tmspro.shop";
+          tags = ["passwords"];
+        }
+        {
+          name = "vaultwarden admin";
+          url = "https://vault.tmspro.shop/admin";
+          tags = ["passwords"];
+        }
+        {
+          name = "weclapp test";
+          url = "https://weclapp-test.tmspro.shop/";
+          tags = ["erp"];
+        }
+        {
+          name = "kanboard";
+          url = "https://kanboard.tmspro.shop/";
+          tags = ["tasks"];
+        }
+        {
+          name = "weclapp test (update)";
+          url = "https://update.weclapp.tmspro.shop/";
+          tags = ["erp"];
+        }
+        {
+          name = "weclapp";
+          url = "https://weclapp.tmspro.shop/";
+          tags = ["erp"];
+        }
+        {
+          name = "bildnamesliste";
+          url = "https://bnl.tmspro.shop/";
+          tags = ["tools"];
+        }
+        {
+          name = "sherlock";
+          url = "https://sherlock.tmspro.shop/";
+          tags = ["tools"];
+        }
+        {
+          name = "toja v2";
+          url = "https://toja.tmspro.shop/";
+          tags = ["tools" "beta"];
+        }
+        {
+          name = "toja";
+          url = "https://toja-prod.tmspro.shop/";
+          tags = ["tools"];
+        }
+        {
+          name = "anna-auftrag";
+          url = "https://anna-auftrag.tmspro.shop/";
+          tags = ["tools"];
         }
 
         {
-          name = "maxcluster";
-          url = "https://app.maxcluster.de/";
-          tags = ["hosting" "magento" "staging"];
-        }
-        {
-          name = "hetzner-console";
-          url = "https://console.hetzner.com";
-          tags = ["hosting" "magento" "staging"];
+          name = "timas";
+          url = "https://timas.tmspro.shop/";
+          tags = ["payroll"];
         }
 
+        {
+          name = "jira";
+          url = "https://tmsproshop.atlassian.net/jira";
+          tags = ["tasks"];
+        }
+        {
+          name = "confluence";
+          url = "https://tmsproshop.atlassian.net/wiki";
+          tags = ["docs"];
+        }
+        {
+          name = "bitbucket";
+          url = "bitbucket.org/tmsproshop";
+          tags = ["source" "git"];
+        }
+        {
+          name = "magento repo";
+          url = "bitbucket.org/tmsproshop";
+          tags = ["source" "git" "project"];
+        }
         {
           name = "grafana";
           url = "https://grafana.tmspro.shop";
@@ -97,124 +167,82 @@
         {
           name = "mailpit";
           url = "https://mailpit.tmspro.shop";
-          tags = ["smtp" "mail relay"];
+          tags = ["mail relay"];
         }
         {
-          name = "github tms";
+          name = "github org";
           url = "https://github.com/orgs/tmsproshopgmbh/repositories";
-          tags = ["vcs" "git" "projects"];
+          tags = ["source" "git" "projects"];
         }
         {
           name = "github infra";
           url = "https://github.com/orgs/tmsproshopgmbh/infra";
-          tags = ["vcs" "git" "repo"];
+          tags = ["source" "git" "repo"];
         }
         {
-          name = "vaultwarden";
-          url = "https://vault.tmspro.shop";
-          tags = ["passwords"];
-        }
-        {
-          name = "weclapp test";
-          url = "https://weclapp-test.tmspro.shop/";
-          tags = ["daily business" "erp"];
-        }
-        {
-          name = "weclapp test (update)";
-          url = "https://update.weclapp.tmspro.shop/";
-          tags = ["daily business" "erp"];
-        }
-        {
-          name = "weclapp";
-          url = "https://weclapp.tmspro.shop/";
-          tags = ["daily business" "erp"];
-        }
-        {
-          name = "bildnamesliste";
-          url = "https://bnl.tmspro.shop/";
-          tags = ["daily business"];
-        }
-        {
-          name = "sherlock";
-          url = "https://sherlock.tmspro.shop/";
-          tags = ["daily business"];
-        }
-        {
-          name = "toja v2";
-          url = "https://toja.tmspro.shop/";
-          tags = ["daily business"];
-        }
-        {
-          name = "toja";
-          url = "https://toja-prod.tmspro.shop/";
-          tags = ["daily business"];
-        }
-        {
-          name = "anna-auftrag";
-          url = "https://anna-auftrag.tmspro.shop/";
-          tags = ["daily business"];
-        }
-        {
-          name = "timas";
-          url = "https://timas.tmspro.shop/";
-          tags = ["daily business"];
-        }
-
-        {
-          name = "jira";
-          url = "https://tmsproshop.atlassian.net/jira";
-          tags = ["atlassian" "todo"];
-        }
-        {
-          name = "confluence";
-          url = "https://tmsproshop.atlassian.net/wiki";
-          tags = ["atlassian" "docs"];
-        }
-        {
-          name = "allinkl";
-          url = "https://kasserver.com/";
-          tags = ["kasserver" "email" "domains" "cdn"];
-        }
-        {
-          name = "tailscale admin";
-          url = "https://login.tailscale.com/admin";
-          tags = ["tailscale" "vpn"];
-        }
-
-        {
-          name = "pve-node-a";
-          url = "https://10.180.80.252:8006";
-          tags = ["proxmox"];
-        }
-        {
-          name = "pve-node-b";
-          url = "https://10.180.80.250:8006";
-          tags = ["proxmox"];
-        }
-        {
-          name = "pve-node-c";
-          url = "https://10.0.10.7:8006";
-          tags = ["proxmox"];
-        }
-        {
-          name = "pfsense";
-          url = "https://10.180.80.254";
-          tags = ["netgate" "firewall"];
-        }
-        {
-          name = "unifi";
-          url = "https://10.180.80.99";
-          tags = ["switch" "network" "ubiquity"];
-        }
-        {
-          name = "pve storage";
-          url = "https://10.180.80.3:5001/";
-          tags = ["synology" "nas" "nfs"];
-        }
-        {
-          name = "smb storage";
-          url = "https://10.102.99.213:5001/";
-          tags = ["synology" "san" "smb"];
+          name = "infra";
+          bookmarks = [
+            {
+              name = "o2business";
+              url = "https://admin.digitalphone.o2business.de/";
+              tags = ["telefonie" "phone"];
+            }
+            {
+              name = "tailscale admin";
+              url = "https://login.tailscale.com/admin";
+              tags = ["tailscale" "vpn"];
+            }
+            {
+              name = "pve-node-a";
+              url = "https://10.180.80.252:8006";
+              tags = ["proxmox" "vms"];
+            }
+            {
+              name = "pve-node-b";
+              url = "https://10.180.80.250:8006";
+              tags = ["proxmox" "vms"];
+            }
+            {
+              name = "pve-node-c";
+              url = "https://10.0.10.7:8006";
+              tags = ["proxmox" "vms"];
+            }
+            {
+              name = "pfsense";
+              url = "https://10.180.80.254";
+              tags = ["firewall" "routing" "network"];
+            }
+            {
+              name = "unifi";
+              url = "https://10.180.80.99";
+              tags = ["switch" "network"];
+            }
+            {
+              name = "pve storage";
+              url = "https://10.180.80.3:5001/";
+              tags = ["synology" "nas" "nfs"];
+            }
+            {
+              name = "smb storage";
+              url = "https://10.102.99.213:5001/";
+              tags = ["synology" "san" "smb"];
+            }
+            {
+              name = "allinkl";
+              url = "https://kasserver.com/";
+              tags = ["kasserver" "mail" "domains" "cdn" "ftp"];
+            }
+            {
+              name = "maxcluster";
+              url = "https://app.maxcluster.de/";
+              tags = ["hosting" "magento" "staging"];
+            }
+            {
+              name = "hetzner-console";
+              url = "https://console.hetzner.com";
+              tags = ["hosting" "magento" "staging"];
+            }
+          ];
         }
       ];
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
@@ -224,7 +252,7 @@
     in {
       bookmarks = {
         force = true;
-        settings = [{inherit bookmarks;}];
+        settings = bookmarks;
       };
       extensions.packages = extensions;
       containersForce = true;
