ef5bd0b0ee3302eb159eda26c91d69a6663cc7e6
cethien <borislaw.sotnikow@gmx.de>
2026-04-03 00:35:59 +0200

    chore: cethien sudo settings

--- modules/nixos/users/cethien.nix
+++ modules/nixos/users/cethien.nix
@@ -5,34 +5,42 @@
 }: let
   inherit (lib) mkIf mkMerge mkDefault;
 in {
-  users.users.cethien = {
-    enable = mkDefault false;
-    isNormalUser = true;
-    extraGroups = mkMerge [
-      ["networkmanager" "wheel"]
-      (mkIf config.deeznuts.net-tools.enable ["nettools"])
-      (mkIf config.hardware.sane.enable ["scanner"])
-      (mkIf config.hardware.uinput.enable ["uinput" "input"])
-      (mkIf config.virtualisation.docker.enable ["docker"])
-      (mkIf config.virtualisation.libvirtd.enable ["libvirtd" "kvm"])
-    ];
-    hashedPassword = "$y$j9T$OqozgiTqzcpfQ9zbKkhvr0$i4lBjUVMF1xpGCOm57m/jXVWt0KVDh/evHHFq9mF7Z4";
-    openssh.authorizedKeys.keys = mkIf config.services.openssh.enable [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKgrZmsUHZn7BAGAl83RUkNejlhJbmLr3lklrlVzy2Zz borislaw.sotnikow@gmx.de"
-    ];
-  };
+  config = {
+    users.users.cethien = {
+      enable = mkDefault false;
+      isNormalUser = true;
+      extraGroups = mkMerge [
+        ["networkmanager" "wheel"]
+        (mkIf config.deeznuts.net-tools.enable ["nettools"])
+        (mkIf config.hardware.sane.enable ["scanner"])
+        (mkIf config.hardware.uinput.enable ["uinput" "input"])
+        (mkIf config.virtualisation.docker.enable ["docker"])
+        (mkIf config.virtualisation.libvirtd.enable ["libvirtd" "kvm"])
+      ];
+      hashedPassword = "$y$j9T$OqozgiTqzcpfQ9zbKkhvr0$i4lBjUVMF1xpGCOm57m/jXVWt0KVDh/evHHFq9mF7Z4";
+      openssh.authorizedKeys.keys = mkIf config.services.openssh.enable [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKgrZmsUHZn7BAGAl83RUkNejlhJbmLr3lklrlVzy2Zz borislaw.sotnikow@gmx.de"
+      ];
+    };
 
-  users.users.bsotnikow = {
-    enable = mkDefault false;
-    isNormalUser = true;
-    extraGroups = mkMerge [
-      ["networkmanager" "wheel"]
-      (mkIf config.deeznuts.net-tools.enable ["nettools"])
-      (mkIf config.hardware.sane.enable ["scanner"])
-      (mkIf config.hardware.uinput.enable ["uinput" "input"])
-      (mkIf config.virtualisation.docker.enable ["docker"])
-      (mkIf config.virtualisation.libvirtd.enable ["libvirtd" "kvm"])
-    ];
-    hashedPassword = "$y$j9T$OqozgiTqzcpfQ9zbKkhvr0$i4lBjUVMF1xpGCOm57m/jXVWt0KVDh/evHHFq9mF7Z4";
+    users.users.bsotnikow = {
+      enable = mkDefault false;
+      isNormalUser = true;
+      extraGroups = mkMerge [
+        ["networkmanager" "wheel"]
+        (mkIf config.deeznuts.net-tools.enable ["nettools"])
+        (mkIf config.hardware.sane.enable ["scanner"])
+        (mkIf config.hardware.uinput.enable ["uinput" "input"])
+        (mkIf config.virtualisation.docker.enable ["docker"])
+        (mkIf config.virtualisation.libvirtd.enable ["libvirtd" "kvm"])
+      ];
+      hashedPassword = "$y$j9T$OqozgiTqzcpfQ9zbKkhvr0$i4lBjUVMF1xpGCOm57m/jXVWt0KVDh/evHHFq9mF7Z4";
+    };
+
+    security.sudo.extraConfig = mkIf config.users.users.cethien.enable ''
+      Defaults timestamp_timeout=30
+      Defaults pwfeedback
+      Defaults insults
+    '';
   };
 }
