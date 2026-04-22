3fed658cfc0390a3658093556db807c8c7180a45
cethien <borislaw.sotnikow@gmx.de>
2026-04-22 19:41:44 +0200

    chore(tower/zen): pins

--- clients/tower-of-power/zen-browser.nix
+++ clients/tower-of-power/zen-browser.nix
@@ -27,17 +27,23 @@
     };
 
     pins = {
-      "WhatsApp" = {
+      "whatsapp" = {
         id = "9d8a8f91-7e29-4688-ae2e-da4e49d4a179";
         url = "https://web.whatsapp.com/";
         isEssential = true;
         position = 101;
       };
-      "Google Calendar" = {
+      "calendar" = {
         id = "591c45e0-737f-47d1-86e8-bf173ce87df9";
         url = "https://calendar.google.com";
         isEssential = true;
         position = 102;
+      };
+
+      "youtube" = {
+        id = "217cf342-d929-419b-9a41-75ed87239d99";
+        url = "https://www.youtube.com/feed/subscriptions";
+        position = 1001;
       };
     };
   in {
