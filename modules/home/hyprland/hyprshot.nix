ed933608dc59999676f594225933aa1dd0375119
cethien <borislaw.sotnikow@gmx.de>
2026-05-26 20:46:08 +0200

    add: text extractor

--- modules/home/hyprland/hyprshot.nix
+++ modules/home/hyprland/hyprshot.nix
@@ -11,10 +11,21 @@ in {
 
     wayland.windowManager.hyprland.settings.bind = let
       cmd = mode: "${pkgs.hyprshot}/bin/hyprshot -z -o ~/Pictures -m ${mode}";
+
+      grim = "${pkgs.grim}/bin/grim";
+      slurp = "${pkgs.slurp}/bin/slurp";
+      tesseract = "${pkgs.tesseract}/bin/tesseract";
+      clip = "${pkgs.wl-clipboard}/bin/wl-copy";
+
+      textExtract = pkgs.writeShellScriptBin "text-extract" ''
+        ${grim} -g "$(${slurp})" - | ${tesseract} - stdout -l deu+eng | ${clip}
+      '';
     in [
       ", Print, exec, ${cmd "output"} -m active"
       "ALT, Print, exec, ${cmd "window"} -m active"
       "SUPER SHIFT, S, exec, ${cmd "region"}"
+
+      "SUPER SHIFT, T, exec, ${textExtract}/bin/text-extract"
     ];
   };
 }
