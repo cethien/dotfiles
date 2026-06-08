1709fa2e44a76280f0d9482f085ade8929593160
cethien <borislaw.sotnikow@gmx.de>
2026-06-08 09:56:17 +0200

    chore: refac

--- modules/_nixos/users/cethien.nix
+++ modules/_nixos/users/cethien.nix
@@ -3,18 +3,20 @@
   config,
   ...
 }: let
-  inherit (lib) mkIf mkMerge mkDefault;
+  inherit (lib) mkIf mkMerge mkDefault optionals;
 in {
   config = {
     users.users.cethien = {
       enable = mkDefault false;
       isNormalUser = true;
-      extraGroups = mkMerge [
+
+      extraGroups =
         ["networkmanager" "wheel"]
-        (mkIf config.virtualisation.docker.enable ["docker"])
-        (mkIf config.virtualisation.libvirtd.enable ["libvirtd" "kvm"])
-      ];
+        ++ optionals config.virtualisation.docker.enable ["docker"]
+        ++ optionals config.virtualisation.libvirtd.enable ["libvirtd" "kvm"];
+
       hashedPassword = "$y$j9T$OqozgiTqzcpfQ9zbKkhvr0$i4lBjUVMF1xpGCOm57m/jXVWt0KVDh/evHHFq9mF7Z4";
+
       openssh.authorizedKeys.keys = mkIf config.services.openssh.enable [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKgrZmsUHZn7BAGAl83RUkNejlhJbmLr3lklrlVzy2Zz borislaw.sotnikow@gmx.de"
       ];
