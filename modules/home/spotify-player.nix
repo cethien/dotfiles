16e7c32d76a0aa48641a87fe67ed9e2229c0823b
cethien <borislaw.sotnikow@gmx.de>
2026-08-03 23:23:44 +0200

    chore: spotify-player daemon wrapper

--- modules/home/spotify-player.nix
+++ modules/home/spotify-player.nix
@@ -6,6 +6,16 @@
 }: let
   inherit (lib) mkIf;
   cfg = config.programs.spotify-player;
+
+  spotify-player-wrapper = pkgs.writeShellScriptBin "spotify-player-wrapper" ''
+    if ! pgrep -f "spotify_player --daemon" >/dev/null; then
+      ${pkgs.spotify-player}/bin/spotify_player --daemon &
+      sleep 1
+    fi
+    exec ${pkgs.spotify-player}/bin/spotify_player "$@"
+  '';
+
+  exec = "${spotify-player-wrapper}/bin/spotify-player-wrapper";
 in {
   config = mkIf cfg.enable {
     programs.spotify-player.settings = {
@@ -16,6 +26,7 @@ in {
         audio_cache = true;
         normalization = true;
       };
+
       border_type = "Rounded";
       playback_format = "{track} {liked}\n{artists}\n{album}\n{status} {metadata}";
       play_icon = "󰐊";
@@ -24,10 +35,18 @@ in {
       explicit_icon = "󰯹";
     };
 
+    programs.tmux.keybindings = [
+      {
+        key = "M";
+        action = "new-window -n 'spotify' '${exec}";
+      }
+    ];
+
     wayland.windowManager.hyprland.extraLuaFiles."99-spotify-player" =
       #lua
       ''
         Modal("spotify_player", {
+            exec = "${exec}";
             binds = { "SUPER + M", "XF86Music" }
         })
 
