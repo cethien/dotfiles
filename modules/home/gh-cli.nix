cf77b3aefb0aae83e2cd16ac2128ce8dee51a7d1
cethien <borislaw.sotnikow@gmx.de>
2026-08-19 00:25:09 +0200

    add(nvim): Octo search

--- modules/home/gh-cli.nix
+++ modules/home/gh-cli.nix
@@ -19,10 +19,21 @@ in {
           		use_emojis = true,
           	},
           })
+
           vim.keymap.set("n", "<leader>oi", "<cmd>Octo issue list<CR>", { desc = "Show issues" })
           vim.keymap.set("n", "<leader>or", "<cmd>Octo review<CR>", { desc = "Show issues" })
           vim.keymap.set("n", "<leader>op", "<cmd>Octo pr list<CR>", { desc = "Show PRs" })
           vim.keymap.set("n", "<leader>od", "<cmd>Octo discussion list<CR>", { desc = "Show discussions" })
+
+          vim.keymap.set("n", "<leader>os", function()
+          	vim.ui.input({ prompt = "Octo Search: " }, function(input)
+          		if not input or input == "" then
+          			return
+          		end
+
+          		vim.cmd("Octo search " .. input)
+          	end)
+          end, { desc = "Interactive Octo Search" })
         '';
     };
   };
