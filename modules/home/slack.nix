c4b1cc931722f2142045e2a5d9095ee17cb6e58a
cethien <borislaw.sotnikow@gmx.de>
2026-09-07 17:47:22 +0200

    chore: slack-password-fix
    chromium...

--- modules/home/slack.nix
+++ modules/home/slack.nix
@@ -8,9 +8,18 @@
   inherit (lib) mkIf mkEnableOption;
   cfg = config.programs.slack;
 
+  slack-password-fix = pkgs.symlinkJoin {
+    name = "slack-password-fix";
+    paths = [pkgs-unstable.slack];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/slack --add-flags "--password-store=basic"
+    '';
+  };
+
   autostartFile = pkgs.makeDesktopItem {
     name = "slack-autostart";
-    exec = "slack -u";
+    exec = "${slack-password-fix}/bin/slack -u";
     desktopName = "Slack (Autostart)";
   };
 in {
@@ -20,7 +29,7 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.packages = [pkgs-unstable.slack];
+    home.packages = [slack-password-fix];
 
     services.mako.settings."app-name=Slack" = {
       default-timeout = 0;
