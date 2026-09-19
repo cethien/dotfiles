9e217143d08eddf356703f798b831d47eea4c63e
cethien <borislaw.sotnikow@gmx.de>
2026-09-19 14:36:53 +0200

    add: dnd mode

--- modules/home/hyprland/mako.nix
+++ modules/home/hyprland/mako.nix
@@ -15,12 +15,37 @@ in {
         #lua
         ''
           hl.bind("SUPER + A", hl.dsp.exec_cmd("makoctl dismiss -a"))
+
+          local function toggle_dnd()
+          	-- Fetch current mode
+          	local handle = io.popen("makoctl mode")
+          	local raw_mode = handle and handle:read("*a") or ""
+          	if handle then
+          		handle:close()
+          	end
+
+          	-- Check if 'dnd' exists in the raw output stream
+          	if raw_mode:find("dnd") then
+          		os.execute("makoctl mode -s default")
+          		swayosd("--custom-icon notifications-active --custom-message 'Notifications Enabled'")
+          	else
+          		os.execute("makoctl mode -s dnd")
+          		swayosd("--custom-icon notifications-disabled --custom-message 'Do Not Disturb'")
+          	end
+          end
+
+          hl.bind("SUPER + SHIFT + A", toggle_dnd)
         '';
     };
 
     services.mako.settings = {
       "urgency=critical" = {
         border-color = "#ff0000";
+        default-timeout = 0;
+      };
+
+      "mode=dnd" = {
+        invisible = 1;
         default-timeout = 0;
       };
 
